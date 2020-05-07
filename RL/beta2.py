1c504748e50901c24756e4cf8b1a24099539f990
Yonv <37322666+Yonv1943@users.noreply.github.com>
2020-05-07 18:43:01 +0800

    SAC update save

--- RL/beta2.py
+++ RL/beta2.py
@@ -2,17 +2,13 @@ import argparse
 import random
 import copy
 import os
-from abc import ABC
-
 import gym
 import numpy as np
 import torch
 import torch.nn as nn
+import torch.nn.functional as F
 from torch.distributions.normal import Normal
 from torch.distributions import Distribution
-
-from AgentNetwork import LinearNet, layer_norm
-from AgentRun import get_env_info
 
 """
 Reference: https://github.com/TianhongDai/reinforcement-learning-algorithms/tree/master/rl_algorithms/sac
@@ -56,9 +52,8 @@ def get_args():
     return parse.parse_args()
 
 
-class TanhNormal(Distribution, ABC):
+class tanh_normal(Distribution):
     def __init__(self, normal_mean, normal_std, epsilon=1e-6, cuda=False):
-        super(object, self).__init__()
         self.normal_mean = normal_mean
         self.normal_std = normal_std
         self.cuda = cuda
@@ -107,10 +102,10 @@ class TanhNormal(Distribution, ABC):
             return torch.tanh(z)
 
 
-class ActionInfo:
+class get_action_info:
     def __init__(self, pis, cuda=False):
         self.mean, self.std = pis
-        self.dist = TanhNormal(normal_mean=self.mean, normal_std=self.std, cuda=cuda)
+        self.dist = tanh_normal(normal_mean=self.mean, normal_std=self.std, cuda=cuda)
 
     # select actions
     def select_actions(self, exploration=True, reparameterize=True):
@@ -129,34 +124,47 @@ class ActionInfo:
         return log_prob.sum(dim=1, keepdim=True)
 
 
-class ActorSAC(nn.Module):  # no tanh
-    def __init__(self, state_dim, action_dim, mid_dim, use_densenet):
-        use_densenet += 1  # backup
-        super(ActorSAC, self).__init__()
-        self.log_std_max = 2.0
-        self.log_std_min = -20.0
+class flatten_mlp(nn.Module):
+    # TODO: add the initialization method for it
+    def __init__(self, input_dims, hidden_size, action_dims=None):
+        super(flatten_mlp, self).__init__()
+        self.fc1 = nn.Linear(input_dims, hidden_size) if action_dims is None else nn.Linear(input_dims + action_dims,
+                                                                                            hidden_size)
+        self.fc2 = nn.Linear(hidden_size, hidden_size)
+        self.q_value = nn.Linear(hidden_size, 1)
 
-        self.net = nn.Sequential(nn.Linear(state_dim, mid_dim), nn.ReLU(),
-                                 LinearNet(mid_dim), )
-        self.net__a_mean = nn.Linear(mid_dim, action_dim)
-        self.net__log_std = nn.Linear(mid_dim, action_dim)
-
-        # layer_norm(self.net[0], std=1.0)
-        layer_norm(self.net__a_mean, std=0.01)
-        self.device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-
-    def forward(self, s, noise_std=0.0):
-        x = self.net(s)
-        a_mean = self.net__a_mean(x)
-        if noise_std:
-            a_log_std = self.net__log_std(x).clamp(self.log_std_min, self.log_std_max)
-            a_std = torch.exp(a_log_std)
-            return a_mean, a_std
-        else:  # noise_std == 0
-            return a_mean
+    def forward(self, obs, action=None):
+        inputs = torch.cat([obs, action], dim=1) if action is not None else obs
+        x = F.relu(self.fc1(inputs))
+        x = F.relu(self.fc2(x))
+        output = self.q_value(x)
+        return output
 
 
-class ReplayBuffer:
+class tanh_gaussian_actor(nn.Module):
+    def __init__(self, input_dims, action_dims, hidden_size, log_std_min, log_std_max):
+        super(tanh_gaussian_actor, self).__init__()
+        self.fc1 = nn.Linear(input_dims, hidden_size)
+        self.fc2 = nn.Linear(hidden_size, hidden_size)
+        self.mean = nn.Linear(hidden_size, action_dims)
+        self.log_std = nn.Linear(hidden_size, action_dims)
+        # the log_std_min and log_std_max
+        self.log_std_min = log_std_min
+        self.log_std_max = log_std_max
+
+    def forward(self, obs):
+        x = F.relu(self.fc1(obs))
+        x = F.relu(self.fc2(x))
+        mean = self.mean(x)
+        log_std = self.log_std(x)
+        # clamp the log std
+        log_std = torch.clamp(log_std, min=self.log_std_min, max=self.log_std_max)
+        # the reparameterization trick
+        # return mean and std
+        return (mean, torch.exp(log_std))
+
+
+class replay_buffer:
     def __init__(self, memory_size):
         self.storge = []
         self.memory_size = memory_size
@@ -191,60 +199,42 @@ class ReplayBuffer:
         return self._encode_sample(idxes)
 
 
-class AgentSAC:
+class sac_agent:
     def __init__(self, env, args):
         self.args = args
         self.env = env
-
-        state_dim, action_dim, max_action, target_reward = get_env_info(env)
-        net_dim = args.hidden_size
-        use_densenet = False
-        use_spectral_norm = False
-        learning_rate = 3e-4
-        self.device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-        self.target_reward = target_reward
-        self.env_name = self.args.env_name
-
         # create eval environment
         self.eval_env = gym.make(self.args.env_name)
         self.eval_env.seed(args.seed * 2)
-
-        from AgentNetwork import Critic
         # build up the network that will be used.
-        self.qf1 = Critic(state_dim, action_dim, net_dim, use_densenet, use_spectral_norm)
-        self.qf2 = Critic(state_dim, action_dim, net_dim, use_densenet, use_spectral_norm)
-        # self.qf1 = CriticValue(self.env.observation_space.shape[0], self.args.hidden_size,
-        #                        self.env.action_space.shape[0])
-        # self.qf2 = CriticValue(self.env.observation_space.shape[0], self.args.hidden_size,
-        #                        self.env.action_space.shape[0])
-
+        self.qf1 = flatten_mlp(self.env.observation_space.shape[0], self.args.hidden_size,
+                               self.env.action_space.shape[0])
+        self.qf2 = flatten_mlp(self.env.observation_space.shape[0], self.args.hidden_size,
+                               self.env.action_space.shape[0])
         # set the target q functions
         self.target_qf1 = copy.deepcopy(self.qf1)
         self.target_qf2 = copy.deepcopy(self.qf2)
         # build up the policy network
-
-        self.act = ActorSAC(state_dim, action_dim, net_dim, use_densenet)
-        # self.actor_net = ActorSAC(self.env.observation_space.shape[0], self.env.action_space.shape[0],
-        #                           self.args.hidden_size, \
-        #                           self.args.log_std_min, self.args.log_std_max)
-
+        self.actor_net = tanh_gaussian_actor(self.env.observation_space.shape[0], self.env.action_space.shape[0],
+                                             self.args.hidden_size, \
+                                             self.args.log_std_min, self.args.log_std_max)
         # define the optimizer for them
-        self.qf1_optim = torch.optim.Adam(self.qf1.parameters(), lr=learning_rate)
-        self.qf2_optim = torch.optim.Adam(self.qf2.parameters(), lr=learning_rate)
+        self.qf1_optim = torch.optim.Adam(self.qf1.parameters(), lr=self.args.q_lr)
+        self.qf2_optim = torch.optim.Adam(self.qf2.parameters(), lr=self.args.q_lr)
         # the optimizer for the policy network
-        self.actor_optim = torch.optim.Adam(self.act.parameters(), lr=learning_rate)
+        self.actor_optim = torch.optim.Adam(self.actor_net.parameters(), lr=self.args.p_lr)
         # entorpy target
         self.target_entropy = -np.prod(self.env.action_space.shape).item()
         self.log_alpha = torch.zeros(1, requires_grad=True, device='cuda' if self.args.cuda else 'cpu')
         # define the optimizer
-        self.alpha_optim = torch.optim.Adam([self.log_alpha], lr=learning_rate)
+        self.alpha_optim = torch.optim.Adam([self.log_alpha], lr=self.args.p_lr)
         # define the replay buffer
-        self.buffer = ReplayBuffer(self.args.buffer_size)
+        self.buffer = replay_buffer(self.args.buffer_size)
         # get the action max
-        self.action_max = max_action
+        self.action_max = self.env.action_space.high[0]
         # if use cuda, put tensor onto the gpu
         if self.args.cuda:
-            self.act.cuda()
+            self.actor_net.cuda()
             self.qf1.cuda()
             self.qf2.cuda()
             self.target_qf1.cuda()
@@ -256,90 +246,32 @@ class AgentSAC:
         if not os.path.exists(self.model_path):
             os.mkdir(self.model_path)
 
-    def select_actions(self, states, explore_noise=0.0):  # CPU array to GPU tensor to CPU array
-        states = torch.tensor(states, dtype=torch.float32, device=self.device)
-        actions = self.act(states)
-
-        if explore_noise == 0.0:
-            actions = actions.cpu().data.numpy()
-            return actions
-        else:
-            a_noise, log_prob = self.act.get__a__log_prob(actions)
-            a_noise = a_noise.cpu().data.numpy()
-
-            log_prob = log_prob.cpu().data.numpy()
-
-            q_value = self.act.critic(states)
-            q_value = q_value.cpu().data.numpy()
-            return a_noise, log_prob, q_value
-
-    def save_or_load_model(self, mod_dir, is_save):
-        act_save_path = '{}/actor.pth'.format(mod_dir)
-        # cri_save_path = '{}/critic.pth'.format(mod_dir)
-
-        if is_save:
-            torch.save(self.act.state_dict(), act_save_path)
-            # torch.save(self.cri.state_dict(), cri_save_path)
-            # print("Saved act and cri:", mod_dir)
-        elif os.path.exists(act_save_path):
-            act_dict = torch.load(act_save_path, map_location=lambda storage, loc: storage)
-            self.act.load_state_dict(act_dict)
-            # self.act_target.load_state_dict(act_dict)
-            # cri_dict = torch.load(cri_save_path, map_location=lambda storage, loc: storage)
-            # self.cri.load_state_dict(cri_dict)
-            # self.cri_target.load_state_dict(cri_dict)
-        else:
-            print("FileNotFound when load_model: {}".format(mod_dir))
-
     # train the agent
     def learn(self):
-        from AgentRun import Recorder
-        max_step = self.args.epoch_length
-        max_action = self.action_max
-        target_reward = self.target_reward
-        env_name = self.env_name
-        recorder = Recorder(self, max_step, max_action, target_reward, env_name)
-        cwd = self.args.save_dir
-
-        qf1_loss = qf2_loss = actor_loss = None
-        # qf1_loss = qf2_loss = actor_loss = alpha = alpha_loss = None
         global_timesteps = 0
         # before the official training, do the initial exploration to add episodes into the replay buffer
         self._initial_exploration(exploration_policy=self.args.init_exploration_policy)
         # reset the environment
-        state = self.env.reset()
-        reward_sum = 0.0
-        step = 0
+        obs = self.env.reset()
         for epoch in range(self.args.n_epochs):
-
-            rewards = list()
-            steps = list()
-
             for _ in range(self.args.train_loop_per_epoch):
                 # for each epoch, it will reset the environment
                 for t in range(self.args.epoch_length):
                     # start to collect samples
                     with torch.no_grad():
-                        obs_tensor = self._get_tensor_inputs(state)
-                        pi = self.act(obs_tensor, True)
-                        action = ActionInfo(pi, cuda=self.args.cuda).select_actions(reparameterize=False)
+                        obs_tensor = self._get_tensor_inputs(obs)
+                        pi = self.actor_net(obs_tensor)
+                        action = get_action_info(pi, cuda=self.args.cuda).select_actions(reparameterize=False)
                         action = action.cpu().numpy()[0]
                     # input the actions into the environment
-                    next_state, reward, done, _ = self.env.step(self.action_max * action)
-                    step += 1
+                    obs_, reward, done, _ = self.env.step(self.action_max * action)
                     # store the samples
-                    self.buffer.add(state, action, reward, next_state, float(done))
+                    self.buffer.add(obs, action, reward, obs_, float(done))
                     # reassign the observations
-                    state = next_state
+                    obs = obs_
                     if done:
-                        rewards.append(reward_sum)
-                        steps.append(step)
-
                         # reset the environment
-                        state = self.env.reset()
-                        reward_sum = 0.0
-                        step = 0
-
+                        obs = self.env.reset()
                 # after collect the samples, start to update the network
                 for _ in range(self.args.update_cycles):
                     qf1_loss, qf2_loss, actor_loss, alpha, alpha_loss = self._update_newtork()
@@ -348,21 +280,13 @@ class AgentSAC:
                         self._update_target_network(self.target_qf1, self.qf1)
                         self._update_target_network(self.target_qf2, self.qf2)
                     global_timesteps += 1
-            # # print the log information
-            # if epoch % self.args.display_interval == 0:
-            #     # start to do the evaluation
-            #     mean_rewards = self._evaluate_agent()
-            #     print('E&R {:4} {:7.2f} |QF1&2 {:7.2f} {:7.2f} |AL {:7.3f} |Alpha&L {:7.3f} {:7.3f}'.format(
-            #         epoch, mean_rewards, qf1_loss, qf2_loss, actor_loss, alpha, alpha_loss))
-            #     torch.save(self.act.state_dict(), self.model_path + '/model.pt')
-            with torch.no_grad():  # just the GPU memory
-                # is_solved = recorder.show_and_check_reward(
-                #     epoch, epoch_reward, iter_num, actor_loss, critic_loss, cwd)
-                critic_loss = (qf1_loss + qf2_loss) * 0.5
-                recorder.show_reward(epoch, rewards, steps, actor_loss, critic_loss)
-                is_solved = recorder.check_reward(cwd, actor_loss, critic_loss)
-                if is_solved:
-                    break
+            # print the log information
+            if epoch % self.args.display_interval == 0:
+                # start to do the evaluation
+                mean_rewards = self._evaluate_agent()
+                print('E&R {} {:7.2f} |QF1&2 {:7.2f} {:7.2f} |AL {:7.3f} |Alpha&L {:7.3f} {:7.3f}'.format(
+                    epoch, mean_rewards, qf1_loss, qf2_loss, actor_loss, alpha, alpha_loss))
+                torch.save(self.actor_net.state_dict(), self.model_path + '/model.pt')
 
     # do the initial exploration by using the uniform policy
     def _initial_exploration(self, exploration_policy='gaussian'):
@@ -376,8 +300,8 @@ class AgentSAC:
                 with torch.no_grad():
                     obs_tensor = self._get_tensor_inputs(obs)
                     # generate the policy
-                    pi = self.act(obs_tensor, True)
-                    action = ActionInfo(pi).select_actions(reparameterize=False)
+                    pi = self.actor_net(obs_tensor)
+                    action = get_action_info(pi).select_actions(reparameterize=False)
                     action = action.cpu().numpy()[0]
                 # input the action input the environment
                 obs_, reward, done, _ = self.env.step(self.action_max * action)
@@ -387,7 +311,7 @@ class AgentSAC:
                 if done:
                     # if done, reset the environment
                     obs = self.env.reset()
-        # print("Initial exploration has been finished!")
+        print("Initial exploration has been finished!")
 
     # get tensors
     def _get_tensor_inputs(self, obs):
@@ -406,8 +330,8 @@ class AgentSAC:
         inverse_dones = torch.tensor(1 - dones, dtype=torch.float32,
                                      device='cuda' if self.args.cuda else 'cpu').unsqueeze(-1)
         # start to update the actor network
-        pis = self.act(obses, True)
-        actions_info = ActionInfo(pis, cuda=self.args.cuda)
+        pis = self.actor_net(obses)
+        actions_info = get_action_info(pis, cuda=self.args.cuda)
         actions_, pre_tanh_value = actions_info.select_actions(reparameterize=True)
         log_prob = actions_info.get_log_prob(actions_, pre_tanh_value)
         # use the automatically tuning
@@ -424,8 +348,8 @@ class AgentSAC:
         q1_value = self.qf1(obses, actions)
         q2_value = self.qf2(obses, actions)
         with torch.no_grad():
-            pis_next = self.act(obses_, True)
-            actions_info_next = ActionInfo(pis_next, cuda=self.args.cuda)
+            pis_next = self.actor_net(obses_)
+            actions_info_next = get_action_info(pis_next, cuda=self.args.cuda)
             actions_next_, pre_tanh_value_next = actions_info_next.select_actions(reparameterize=True)
             log_prob_next = actions_info_next.get_log_prob(actions_next_, pre_tanh_value_next)
             target_q_value_next = torch.min(self.target_qf1(obses_, actions_next_),
@@ -461,9 +385,9 @@ class AgentSAC:
             while True:
                 with torch.no_grad():
                     obs_tensor = self._get_tensor_inputs(obs)
-                    pi = self.act(obs_tensor, True)
-                    action = ActionInfo(pi, cuda=self.args.cuda).select_actions(exploration=False,
-                                                                                reparameterize=False)
+                    pi = self.actor_net(obs_tensor)
+                    action = get_action_info(pi, cuda=self.args.cuda).select_actions(exploration=False,
+                                                                                     reparameterize=False)
                     action = action.detach().cpu().numpy()[0]
                 # input the action into the environment
                 obs_, reward, done, _ = self.eval_env.step(self.action_max * action)
@@ -475,36 +399,9 @@ class AgentSAC:
         return total_reward / self.args.eval_episodes
 
 
-def run__sac(gpu_id=0, cwd='AC_SAC'):
-    os.environ['CUDA_VISIBLE_DEVICES'] = str(gpu_id)
-
+if __name__ == '__main__':
     args = get_args()
     args.env_name = "LunarLanderContinuous-v2"
-    args.save_dir = './{}/LL_{}'.format(cwd, gpu_id)
     env = gym.make(args.env_name)
-    sac_trainer = AgentSAC(env, args)
+    sac_trainer = sac_agent(env, args)
     sac_trainer.learn()
-    env.close()
-
-
-def run__sac_bw(gpu_id=0, cwd='AC_SAC'):
-    os.environ['CUDA_VISIBLE_DEVICES'] = str(gpu_id)
-
-    args = get_args()
-    args.env_name = "BipedalWalker-v3"
-    args.save_dir = './{}/BW_{}'.format(cwd, gpu_id)
-    env = gym.make(args.env_name)
-    sac_trainer = AgentSAC(env, args)
-    sac_trainer.learn()
-    env.close()
-
-
-if __name__ == '__main__':
-    from AgentRun import run__multi_process
-    from AgentRun import run__td3
-
-    run__multi_process(run__td3, gpu_tuple=(0, 1,), cwd='AC_TD3')
-
-    # run__sac(gpu_id=0, cwd='AC_SAC')
-    run__multi_process(run__sac, gpu_tuple=(0, 1,), cwd='AC_SAC')
-    run__multi_process(run__sac_bw, gpu_tuple=(0, 1,), cwd='AC_SAC')
