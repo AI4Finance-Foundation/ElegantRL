f26a4a440625f74bac7c362d0df9a791af2a62ab
YonV1943 曾伊言 <37322666+Yonv1943@users.noreply.github.com>
2020-10-21 10:12:18 +0800

    evaluate agent

--- BetaWarning/AgentTest.py
+++ BetaWarning/AgentTest.py
@@ -56,7 +56,56 @@ def test__replay_buffer():
     print('Memory length of buffer_offline:', buffer_offline.now_len)
 
 
+def test__evaluate_agent():
+    # save_path = '/mnt/sdb1/yonv/code/ElegantRL_cwd/2020-10-10/PPO/MinitaurBulletEnv-v0_33.72'
+    # env_name = "MinitaurBulletEnv-v0"
+    # rl_agent = AgentPPO
+    # net_dim = 2 ** 9
+    # import pybullet_envs  # for python-bullet-gym
+    # dir(pybullet_envs)
+
+    save_path = '/mnt/sdb1/yonv/code/ElegantRL_cwd/2020-10-10/InterSAC/BipedalWalkerHardcore-v3_313.8'
+    env_name = "BipedalWalkerHardcore-v3"
+    rl_agent = AgentInterSAC
+    net_dim = 2 ** 8
+
+    env, state_dim, action_dim, target_reward, if_discrete = build_gym_env(env_name, if_print=False)
+
+    agent = rl_agent(state_dim, action_dim, net_dim)
+    del agent.cri
+    agent.save_or_load_model(save_path, if_save=False)
+    from copy import deepcopy
+    act_cpu = deepcopy(agent.act).to(torch.device("cpu"))
+    act_cpu.eval()
+    [setattr(param, 'requires_grad', False) for param in act_cpu.parameters()]
+
+    reward_item = 0.0
+    device = torch.device("cpu")
+
+    print('start evaluation')
+    # env.render()  # pybullet-gym put env.render() before env.reset()
+    state = env.reset()
+    done = False
+    total_step = 0
+    while not done:
+        s_tensor = torch.tensor((state,), dtype=torch.float32, device=device)
+
+        a_tensor = act_cpu(s_tensor).argmax(dim=1) if if_discrete else act_cpu(s_tensor)
+        action = a_tensor.cpu().data.numpy()[0]
+
+        next_state, reward, done, _ = env.step(action)
+        total_step += 1
+        env.render()
+        if total_step % 100 == 0:
+            print(total_step, reward_item)
+        reward_item += reward
+
+        state = next_state
+    print(total_step, reward_item)
+
+
 if __name__ == '__main__':
     # test__env_quickly()
-    test__replay_buffer()
+    # test__replay_buffer()
+    test__evaluate_agent()
     print('; AgentTest Terminal.')
