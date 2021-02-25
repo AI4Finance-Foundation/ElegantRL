c3b89bd8a1d4ad49e27086fba685b9c8c7db235b
Steven Li <46941629+supersglzc@users.noreply.github.com>
2021-02-25 13:05:49 +0800

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,46 @@
+from setuptools import setup, find_packages
+
+# Read requirements.txt, ignore comments
+try:
+    REQUIRES = list()
+    f = open("requirements.txt", "rb")
+    for line in f.read().decode("utf-8").split("\n"):
+        line = line.strip()
+        if "#" in line:
+            line = line[: line.find("#")].strip()
+        if line:
+            REQUIRES.append(line)
+except:
+    print("'requirements.txt' not found!")
+    REQUIRES = list()
+
+setup(
+    name="ElegantRL",
+    version="0.0.1",
+    include_package_data=True,
+    author="author",
+    author_email="author_email",
+    url="https://github.com/AI4Finance-LLC/ElegantRL",
+    license="MIT",
+    packages=find_packages(),
+    # install_requires=REQUIRES,
+    install_requires=REQUIRES
+    + ["pyfolio @ git+https://github.com/quantopian/pyfolio.git#egg=pyfolio-0.9.2"],
+    # dependency_links=['git+https://github.com/quantopian/pyfolio.git#egg=pyfolio-0.9.2'],
+    description="Lightweight, Efficient and Stable DRL Implementation Using PyTorch",
+    classifiers=[
+        # Trove classifiers
+        # Full list: https://pypi.python.org/pypi?%3Aaction=list_classifiers
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: Implementation :: CPython",
+        "Programming Language :: Python :: Implementation :: PyPy",
+    ],
+    keywords="Deep Reinforcment Learning",
+    platform=["any"],
+    python_requires=">=3.6",
+)
