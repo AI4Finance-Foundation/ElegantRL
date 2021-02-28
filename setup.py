dbedcb6ae977bc08ae5a9a4034e0cf615e6d1a2d
Bruce Yang <hongyang.yang@columbia.edu>
2021-03-01 00:53:43 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ setup(
     classifiers=[
         # Trove classifiers
         # Full list: https://pypi.python.org/pypi?%3Aaction=list_classifiers
-        "License :: OSI Approved :: MIT License",
+        "License :: OSI Approved :: Apache Software License",
         "Programming Language :: Python",
         "Programming Language :: Python :: 3",
         "Programming Language :: Python :: 3.6",
