b8f1a90f3969bfa08ba9e1402aa9506d531c51d3
Bruce Yang <hongyang.yang@columbia.edu>
2021-02-28 23:45:39 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,29 +1,31 @@
 from setuptools import setup, find_packages
 # Read requirements.txt, ignore comments
-try:
-    REQUIRES = list()
-    f = open("requirements.txt", "rb")
-    for line in f.read().decode("utf-8").split("\n"):
-        line = line.strip()
-        if "#" in line:
-            line = line[: line.find("#")].strip()
-        if line:
-            REQUIRES.append(line)
+#try:
+#    REQUIRES = list()
+#    f = open("requirements.txt", "rb")
+#    for line in f.read().decode("utf-8").split("\n"):
+#        line = line.strip()
+#        if "#" in line:
+#            line = line[: line.find("#")].strip()
+#        if line:
+#            REQUIRES.append(line)
     # print(REQUIRES)
-except:
-    print("'requirements.txt' not found!")
-    REQUIRES = list()
+#except:
+#    print("'requirements.txt' not found!")
+#    REQUIRES = list()
 setup(
     name="elegantrl",
     version="0.0.1",
-    include_package_data=True,
+    #include_package_data=True,
     author="author",
     author_email="author_email",
     url="https://github.com/AI4Finance-LLC/ElegantRL",
     license="MIT",
-    packages=find_packages(),
+    #packages=find_packages(),
     # install_requires=REQUIRES,
-    install_requires=REQUIRES,
+    #install_requires=REQUIRES,
+    install_requires=[
+        'gym', 'matplotlib', 'numpy', 'pybullet', 'torch'],
     description="Lightweight, Efficient and Stable DRL Implementation Using PyTorch",
     classifiers=[
         # Trove classifiers
