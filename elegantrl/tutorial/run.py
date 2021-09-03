1ce6352bea9a5500efa5914e77715789569f933f
Xiao-Yang Liu <xl2427@columbia.edu>
2021-09-03 00:21:26 -0400

    Update run.py

--- elegantrl/tutorial/run.py
+++ elegantrl/tutorial/run.py
@@ -2,49 +2,49 @@ import gym  # not necessary
 import time
 from elegantrl.tutorial.agent import *
 
-gym.logger.set_level(40)  # Block warning
+gym.logger.set_level(40)                    # Block warning
 
 
 class Arguments:
     def __init__(self, agent=None, env=None, if_on_policy=False):
-        self.agent = agent  # Deep Reinforcement Learning algorithm
-        self.env = env  # the environment for training
+        self.agent = agent                  # Deep Reinforcement Learning algorithm
+        self.env = env                      # the environment for training
 
-        self.cwd = None  # current work directory. None means set automatically
-        self.if_remove = True  # remove the cwd folder? (True, False, None:ask me)
-        self.break_step = 2 ** 20  # break training after 'total_step > break_step'
-        self.if_allow_break = True  # allow break training when reach goal (early termination)
+        self.cwd = None                     # current work directory. None means set automatically
+        self.if_remove = True               # remove the cwd folder? (True, False, None:ask me)
+        self.break_step = 2 ** 20           # break training after 'total_step > break_step'
+        self.if_allow_break = True          # allow break training when reach goal (early termination)
 
-        self.visible_gpu = '0'  # for example: os.environ['CUDA_VISIBLE_DEVICES'] = '0, 2,'
-        self.worker_num = 2  # rollout workers number pre GPU (adjust it to get high GPU usage)
-        self.num_threads = 8  # cpu_num for evaluate model, torch.set_num_threads(self.num_threads)
+        self.visible_gpu = '0'              # for example: os.environ['CUDA_VISIBLE_DEVICES'] = '0, 2,'
+        self.worker_num = 2                 # rollout workers number pre GPU (adjust it to get high GPU usage)
+        self.num_threads = 8                # cpu_num for evaluate model, torch.set_num_threads(self.num_threads)
 
         '''Arguments for training'''
-        self.gamma = 0.99  # discount factor of future rewards
-        self.reward_scale = 2 ** 0  # an approximate target reward usually be closed to 256
-        self.learning_rate = 2 ** -14  # 2 ** -14 ~= 6e-5
-        self.soft_update_tau = 2 ** -8  # 2 ** -8 ~= 5e-3
+        self.gamma = 0.99                   # discount factor of future rewards
+        self.reward_scale = 2 ** 0          # an approximate target reward usually be closed to 256
+        self.learning_rate = 2 ** -14       # 2 ** -14 ~= 6e-5
+        self.soft_update_tau = 2 ** -8      # 2 ** -8 ~= 5e-3
 
-        if if_on_policy:  # (on-policy)
-            self.net_dim = 2 ** 9  # the network width
+        if if_on_policy:                    # (on-policy)
+            self.net_dim = 2 ** 9           # the network width
             self.batch_size = self.net_dim * 2  # num of transitions sampled from replay buffer.
-            self.repeat_times = 2 ** 3  # collect target_step, then update network
-            self.target_step = 2 ** 12  # repeatedly update network to keep critic's loss small
+            self.repeat_times = 2 ** 3      # collect target_step, then update network
+            self.target_step = 2 ** 12      # repeatedly update network to keep critic's loss small
             self.max_memo = self.target_step  # capacity of replay buffer
-            self.if_per_or_gae = False  # GAE for on-policy sparse reward: Generalized Advantage Estimation.
+            self.if_per_or_gae = False      # GAE for on-policy sparse reward: Generalized Advantage Estimation.
         else:
-            self.net_dim = 2 ** 8  # the network width
+            self.net_dim = 2 ** 8           # the network width
             self.batch_size = self.net_dim  # num of transitions sampled from replay buffer.
-            self.repeat_times = 2 ** 0  # repeatedly update network to keep critic's loss small
-            self.target_step = 2 ** 10  # collect target_step, then update network
-            self.max_memo = 2 ** 20  # capacity of replay buffer
-            self.if_per_or_gae = False  # PER for off-policy sparse reward: Prioritized Experience Replay.
+            self.repeat_times = 2 ** 0      # repeatedly update network to keep critic's loss small
+            self.target_step = 2 ** 10      # collect target_step, then update network
+            self.max_memo = 2 ** 20         # capacity of replay buffer
+            self.if_per_or_gae = False      # PER for off-policy sparse reward: Prioritized Experience Replay.
 
         '''Arguments for evaluate'''
-        self.eval_env = None  # the environment for evaluating. None means set automatically.
-        self.eval_gap = 2 ** 6  # evaluate the agent per eval_gap seconds
-        self.eval_times = 2  # number of times that get episode return in first
-        self.random_seed = 0  # initialize random seed in self.init_before_training()
+        self.eval_env = None                # the environment for evaluating. None means set automatically.
+        self.eval_gap = 2 ** 6              # evaluate the agent per eval_gap seconds
+        self.eval_times = 2                 # number of times that get episode return in first
+        self.random_seed = 0                # initialize random seed in self.init_before_training()
 
     def init_before_training(self, if_main):
         if self.cwd is None:
