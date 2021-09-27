6a3b7824d3928c831d2f73905854927fa975a2b9
YonV1943 曾伊言 <37322666+Yonv1943@users.noreply.github.com>
2021-09-27 16:57:27 +0800

    Full code: Run IsaacGym Fix Evaluator Fix vec_env

--- elegantrl/AgentZoo/ElegantRL-Isaac/beta1.py
+++ elegantrl/AgentZoo/ElegantRL-Isaac/beta1.py
@@ -1,113 +1,26 @@
+from envs.IsaacGym import *
 from elegantrl.demo import *
-from envs.IsaacGym import *  # todo isaac
-
-
-class Arguments:
-    def __init__(self, if_on_policy=False):
-        self.env = None  # the environment for training
-        self.agent = None  # Deep Reinforcement Learning algorithm
-
-        '''Arguments for training'''
-        self.gamma = 0.99  # discount factor of future rewards
-        self.reward_scale = 2 ** 0  # an approximate target reward usually be closed to 256
-        self.learning_rate = 2 ** -15  # 2 ** -14 ~= 3e-5
-        self.soft_update_tau = 2 ** -8  # 2 ** -8 ~= 5e-3
-
-        self.if_on_policy = if_on_policy
-        if self.if_on_policy:  # (on-policy)
-            self.net_dim = 2 ** 9  # the network width
-            self.batch_size = self.net_dim * 2  # num of transitions sampled from replay buffer.
-            self.repeat_times = 2 ** 3  # collect target_step, then update network
-            self.target_step = 2 ** 12  # repeatedly update network to keep critic's loss small
-            self.max_memo = self.target_step  # capacity of replay buffer
-            self.if_per_or_gae = False  # GAE for on-policy sparse reward: Generalized Advantage Estimation.
-        else:
-            self.net_dim = 2 ** 8  # the network width
-            self.batch_size = self.net_dim  # num of transitions sampled from replay buffer.
-            self.repeat_times = 2 ** 0  # repeatedly update network to keep critic's loss small
-            self.target_step = 2 ** 10  # collect target_step, then update network
-            self.max_memo = 2 ** 21  # capacity of replay buffer
-            self.if_per_or_gae = False  # PER for off-policy sparse reward: Prioritized Experience Replay.
-
-        '''Arguments for device'''
-        self.env_num = 1  # The Environment number for each worker. env_num == 1 means don't use VecEnv.
-        self.worker_num = 2  # rollout workers number pre GPU (adjust it to get high GPU usage)
-        self.thread_num = 8  # cpu_num for evaluate model, torch.set_num_threads(self.num_threads)
-        self.visible_gpu = '0'  # for example: os.environ['CUDA_VISIBLE_DEVICES'] = '0, 2,'
-        self.random_seed = 0  # initialize random seed in self.init_before_training()
-
-        '''Arguments for evaluate and save'''
-        self.cwd = None  # current work directory. None means set automatically
-        self.if_remove = True  # remove the cwd folder? (True, False, None:ask me)
-        self.break_step = 2 ** 20  # break training after 'total_step > break_step'
-        self.if_allow_break = True  # allow break training when reach goal (early termination)
-
-        self.eval_env = None  # the environment for evaluating. None means set automatically.
-        self.eval_gap = 2 ** 7  # evaluate the agent per eval_gap seconds
-        self.eval_times1 = 2 ** 3  # number of times that get episode return in first
-        self.eval_times2 = 2 ** 4  # number of times that get episode return in second
-        self.eval_device_id = -1  # -1 means use cpu, >=0 means use GPU
-
-    def init_before_training(self, if_main):
-        np.random.seed(self.random_seed)
-        torch.manual_seed(self.random_seed)
-        torch.set_num_threads(self.thread_num)
-        torch.set_default_dtype(torch.float32)
-
-        # os.environ['CUDA_VISIBLE_DEVICES'] = str(self.visible_gpu)
-        os.environ['CUDA_VISIBLE_DEVICES'] = '1,2,3,4,5'  # todo
-
-        '''env'''
-        if self.env is None:
-            raise RuntimeError(f'\n| Why env=None? For example:'
-                               f'\n| args.env = XxxEnv()'
-                               f'\n| args.env = str(env_name)'
-                               f'\n| args.env = build_env(env_name), from elegantrl.env import build_env')
-        if not (isinstance(self.env, str) or hasattr(self.env, 'env_name')):
-            raise RuntimeError('\n| What is env.env_name? use env=PreprocessEnv(env).')
-
-        '''agent'''
-        if self.agent is None:
-            raise RuntimeError(f'\n| Why agent=None? Assignment `args.agent = AgentXXX` please.')
-        if not hasattr(self.agent, 'init'):
-            raise RuntimeError(f"\n| why hasattr(self.agent, 'init') == False"
-                               f'\n| Should be `agent=AgentXXX()` instead of `agent=AgentXXX`.')
-        if self.agent.if_on_policy != self.if_on_policy:
-            raise RuntimeError(f'\n| Why bool `if_on_policy` is not consistent?'
-                               f'\n| self.if_on_policy: {self.if_on_policy}'
-                               f'\n| self.agent.if_on_policy: {self.agent.if_on_policy}')
-
-        '''cwd'''
-        if self.cwd is None:
-            agent_name = self.agent.__class__.__name__
-            env_name = getattr(self.env, 'env_name', self.env)
-            self.cwd = f'./{agent_name}_{env_name}_{self.visible_gpu}'
-        if if_main:
-            # remove history according to bool(if_remove)
-            if self.if_remove is None:
-                self.if_remove = bool(input(f"| PRESS 'y' to REMOVE: {self.cwd}? ") == 'y')
-            elif self.if_remove:
-                import shutil
-                shutil.rmtree(self.cwd, ignore_errors=True)
-                print(f"| Remove cwd: {self.cwd}")
-            os.makedirs(self.cwd, exist_ok=True)
 
 
 def demo_isaac_on_policy():
     args = Arguments(if_on_policy=True)  # hyper-parameters of on-policy is different from off-policy
     args.agent = AgentPPO()
-    args.visible_gpu = '3'
     args.random_seed += 1943
 
     if_train_ant = 1
     if if_train_ant:
+        # env = build_env('IsaacOneEnvAnt', if_print=True, device_id=0, env_num=1)
         args.eval_env = 'IsaacOneEnvAnt'
+        args.eval_gpu_id = 7
+
+        # env = build_env('IsaacVecEnvAnt', if_print=True, device_id=0, env_num=2)
         args.env = 'IsaacVecEnvAnt'
-        args.env_num = 16  # from elegantrl.envs import build_isaac_gym_env env_num = 16
-        # args.env_num = 32  # from envs.IsaacGym import PreprocessIsaacVecEnv __init__(env_num, ...)
+        args.env_num = 512
+        args.max_step = 1000
         args.state_dim = 60
         args.action_dim = 8
         args.if_discrete = False
+        args.target_return = 4000
 
         args.agent.lambda_entropy = 0.05
         args.agent.lambda_gae_adv = 0.97
@@ -119,7 +32,7 @@ def demo_isaac_on_policy():
         args.repeat_times = 2 ** 3
         args.net_dim = 2 ** 9
         args.batch_size = args.net_dim * 2 ** 3
-        args.target_step = 2 ** (10 + 1)
+        args.target_step = 2 ** 9  # todo
 
         args.break_step = int(2e7)
         args.if_allow_break = False
@@ -148,7 +61,11 @@ def demo_isaac_on_policy():
         args.break_step = int(8e7)
         args.if_allow_break = False
 
+    args.init_before_training()
+
     # train_and_evaluate(args)
+    args.workers_gpus = (1, )
+    args.learner_gpus = (1, )
     args.worker_num = 1
     train_and_evaluate_mp(args)
 
--- elegantrl/AgentZoo/ElegantRL-Isaac/beta2.py
+++ elegantrl/AgentZoo/ElegantRL-Isaac/beta2.py
@@ -1,167 +1,38 @@
-from beta0 import *
+from envs.IsaacGym import *
 from elegantrl.demo import *
 
 
-def demo_pybullet_off_policy():
-    args = Arguments(if_on_policy=False)  # hyper-parameters of on-policy is different from off-policy
-    args.agent = AgentModSAC()
-    args.visible_gpu = '0'
-    args.random_seed += 19431
+def demo_isaac_on_policy():
+    args = Arguments(if_on_policy=True)  # hyper-parameters of on-policy is different from off-policy
+    args.agent = AgentPPO()
+    args.random_seed += 1943
 
     if_train_ant = 1
     if if_train_ant:
-        args.env = build_env(env='AntBulletEnv-v0', if_print=True)
-        """
-        0  4.29e+06 2446.47 |  431.34   82.1    999     0 |    0.08   1.65-275.32   0.26 | UsedTime:   14393 |
-        0  1.41e+07 3499.37 | 3317.42    5.9    999     0 |    0.24   0.06 -49.94   0.03 | UsedTime:   70020 |
-        0  3.54e+06 2875.30 |  888.67    4.7    999     0 |    0.19   0.11 -69.10   0.05 | UsedTime:   54701 |
-        0  2.00e+07 2960.38 |  698.58   42.5    999     0 |    0.08   0.05 -39.44   0.03 | UsedTime:   53545 |
-        """
-        args.agent.if_use_act_target = False
+        # env = build_env('IsaacOneEnvAnt', if_print=True, device_id=0, env_num=1)
+        args.eval_env = 'IsaacOneEnvAnt'
+        args.eval_gpu_id = 7
 
+        # env = build_env('IsaacVecEnvAnt', if_print=True, device_id=0, env_num=2)
+        args.env = 'IsaacVecEnvAnt'
+        args.env_num = 1024
+        args.max_step = 1000
+        args.state_dim = 60
+        args.action_dim = 8
+        args.if_discrete = False
+        args.target_return = 4000
+
+        args.agent.lambda_entropy = 0.05
+        args.agent.lambda_gae_adv = 0.97
+        args.learning_rate = 2 ** -15
+        args.if_per_or_gae = True
         args.break_step = int(8e7)
 
-        args.reward_scale = 2 ** -2
-        args.net_dim = 2 ** 9
-        args.batch_size = args.net_dim * 2
-        args.max_memo = 2 ** 22
+        args.reward_scale = 2 ** -2  # (-50) 0 ~ 2500 (3340)
         args.repeat_times = 2 ** 1
-        args.target_step = args.env.max_step * 2
-
-        args.break_step = int(2e7)
-        args.if_allow_break = False
-
-        args.eval_gap = 2 ** 8
-        args.eval_times1 = 2 ** 1
-        args.eval_times2 = 2 ** 4
-
-    if_train_humanoid = 0
-    if if_train_humanoid:
-        args.env = build_env(env='HumanoidBulletEnv-v0', if_print=True)
-        """
-        0  1.50e+07 2571.46 |   53.63   66.8    128    58 |    0.04   0.96-153.29   0.06 | UsedTime:    74470 |
-        0  1.51e+07 2822.93 |   -1.51   27.1     99    36 |    0.03   0.58 -96.48   0.04 | UsedTime:    74480 |
-        0  1.09e+06   66.96 |   58.69    8.2     58    12 |    0.22   0.28 -22.92   0.00
-        0  3.01e+06  129.69 |  101.39   40.6     96    33 |    0.14   0.28 -20.16   0.03
-        0  5.02e+06  263.13 |  208.69  122.6    195    59 |    0.11   0.29 -32.71   0.03
-        0  6.03e+06  791.89 |  527.79  282.7    360   144 |    0.21   0.26 -36.51   0.03
-        0  8.00e+06 2432.21 |   35.78   49.3    113    54 |   -0.08   1.30-168.28   0.05
-        0  8.13e+06 2432.21 |  907.28  644.9    606   374 |    0.11   0.72-134.01   0.05
-        0  8.29e+06 2432.21 | 2341.30   39.4    999     0 |    0.41   0.41 -96.96   0.03
-        0  1.09e+07 2936.10 | 2936.10   24.8    999     0 |    0.60   0.13 -68.74   0.02
-        0  2.83e+07 2968.08 | 2737.18   15.9    999     0 |    0.57   0.21 -81.07   0.03 | UsedTime:    74512 |
-        """
-        args.break_step = int(8e7)
-
-        args.reward_scale = 2 ** -2
-        args.net_dim = 2 ** 9
-        args.batch_size = args.net_dim * 2
-        args.target_step = args.env.max_step * 2
-
-        args.break_step = int(8e7)
-        args.if_allow_break = False
-
-    if_train_reacher = 0
-    if if_train_reacher:
-        args.env = build_env(env='ReacherBulletEnv-v0', if_print=True)
-
-        args.explore_rate = 0.9
-        args.learning_rate = 2 ** -15
-
-        args.gamma = 0.99
-        args.reward_scale = 2 ** 2
-        args.break_step = int(4e7)
-
-        args.net_dim = 2 ** 8
-        args.batch_size = args.net_dim * 2
-        args.repeat_times = 2 ** 0
-
-        args.target_step = args.env.max_step * 4
-
-        args.eval_gap = 2 ** 8
-        args.eval_times1 = 2 ** 3
-        args.eval_times2 = 2 ** 5
-
-    if_train_minitaur = 0
-    if if_train_minitaur:
-        args.env = build_env(env='MinitaurBulletEnv-v0', if_print=True)
-        """
-        0  1.00e+06    0.46 |    0.24    0.0     98    37 |    0.06   0.06  -7.64   0.02
-        0  1.26e+06    1.36 |    1.36    0.7    731   398 |    0.10   0.08 -10.40   0.02
-        0  1.30e+06    3.18 |    3.18    0.8    999     0 |    0.13   0.08 -10.99   0.02
-        0  2.00e+06    3.18 |    0.04    0.0     28     0 |    0.13   0.09 -16.02   0.02
-        0  4.04e+06    7.11 |    6.68    0.6    999     0 |    0.17   0.08 -19.67   0.02
-        0  5.72e+06    9.79 |    9.28    0.1    999     0 |    0.22   0.03 -23.89   0.01
-        0  6.01e+06   10.69 |   10.09    0.8    999     0 |    0.22   0.03 -24.98   0.01
-        """
-
-        args.learning_rate = 2 ** -16
-        args.break_step = int(8e7)
-
-        args.reward_scale = 2 ** 5  # (-2) 0 ~ 16 (20)
-        args.net_dim = 2 ** 9
-        args.batch_size = args.net_dim * 2
-        args.target_step = args.env.max_step * 2
-
-        args.break_step = int(4e7)
-        args.if_allow_break = False
-
-        args.eval_gap = 2 ** 8
-        args.eval_times1 = 2 ** 2
-        args.eval_times2 = 2 ** 4
-
-    # train_and_evaluate(args)
-    args.worker_num = 4
-    train_and_evaluate_mp(args)
-
-
-def demo_pybullet_on_policy():
-    args = Arguments(if_on_policy=True)  # hyper-parameters of on-policy is different from off-policy
-    args.agent = AgentPPO()
-    args.visible_gpu = '2'  # todo
-    args.random_seed += 1943
-
-    if_train_ant = 0
-    if if_train_ant:
-        args.env = build_env(env='AntBulletEnv-v0', if_print=True)
-        """
-        0  1.98e+07 3322.16 | 3322.16   48.7    999     0 |    0.78   0.48  -0.01  -0.80 | UsedTime: 12380 PPO
-        0  1.99e+07 3104.05 | 3071.44   14.5    999     0 |    0.74   0.47   0.01  -0.79 | UsedTime: 12976
-        0  1.98e+07 3246.79 | 3245.98   25.3    999     0 |    0.75   0.48  -0.02  -0.81 | UsedTime: 13170
-        0  1.97e+07 3345.48 | 3345.48   29.0    999     0 |    0.80   0.49  -0.01  -0.81 | UsedTime: 8169  PPO 2GPU
-        0  1.98e+07 3028.69 | 3004.67   10.3    999     0 |    0.72   0.48   0.05  -0.82 | UsedTime: 8734  PPO 2GPU
-        """
-
-        args.agent.lambda_entropy = 0.05
-        args.agent.lambda_gae_adv = 0.97
-        args.learning_rate = 2 ** -15
-        args.if_per_or_gae = True
-        args.break_step = int(8e7)
-
-        args.reward_scale = 2 ** -2  # (-50) 0 ~ 2500 (3340)
-        args.repeat_times = 2 ** 3
         args.net_dim = 2 ** 9
         args.batch_size = args.net_dim * 2 ** 3
-        args.target_step = args.env.max_step * 2
-
-        args.break_step = int(2e7)
-        args.if_allow_break = False
-
-    if_train_isaac_ant = 1
-    if if_train_isaac_ant:
-        args.env = build_env(env='IsaacGymAnt', if_print=True)
-
-        args.agent.lambda_entropy = 0.05
-        args.agent.lambda_gae_adv = 0.97
-        args.learning_rate = 2 ** -15
-        args.if_per_or_gae = True
-        args.break_step = int(8e7)
-
-        args.reward_scale = 2 ** -2  # (-50) 0 ~ 2500 (3340)
-        args.repeat_times = 2 ** 3
-        args.net_dim = 2 ** 9
-        args.batch_size = args.net_dim * 2 ** 3
-        args.target_step = args.env.max_step * 2
+        args.target_step = 2 ** 9  # todo
 
         args.break_step = int(2e7)
         args.if_allow_break = False
@@ -190,10 +61,14 @@ def demo_pybullet_on_policy():
         args.break_step = int(8e7)
         args.if_allow_break = False
 
-    train_and_evaluate(args)
-    # args.worker_num = 1
-    # train_and_evaluate_mp(args)
+    args.init_before_training()
+
+    # train_and_evaluate(args)
+    args.workers_gpus = (3, 4)
+    args.learner_gpus = (3, 4)
+    args.worker_num = 1
+    train_and_evaluate_mp(args)
 
 
 if __name__ == '__main__':
-    demo_pybullet_on_policy()
+    demo_isaac_on_policy()
