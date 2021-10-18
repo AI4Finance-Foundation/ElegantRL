26d0f1d06033f937ad0eba0776dc0a09290b14cf
Steven Li <46941629+supersglzc@users.noreply.github.com>
2021-10-18 16:54:08 -0400

    Update agent.py

--- elegantrl/agent.py
+++ elegantrl/agent.py
@@ -562,8 +562,22 @@ class AgentDDPG(AgentBase):
 
 
 class AgentTD3(AgentBase):
-    def __init__(self):
-        super().__init__()
+    """
+    Bases: ``elegantrl.agent.AgentBase``
+    
+    Twin Delayed DDPG algorithm. “Addressing Function Approximation Error in Actor-Critic Methods”. Scott Fujimoto. et al.. 2015.
+    
+    :param net_dim[int]: the dimension of networks (the width of neural networks)
+    :param state_dim[int]: the dimension of state (the number of state vector)
+    :param action_dim[int]: the dimension of action (the number of discrete action)
+    :param learning_rate[float]: learning rate of optimizer
+    :param if_per_or_gae[bool]: PER (off-policy) or GAE (on-policy) for sparse reward
+    :param env_num[int]: the env number of VectorEnv. env_num == 1 means don't use VectorEnv
+    :param agent_id[int]: if the visible_gpu is '1,9,3,4', agent_id=1 means (1,9,4,3)[agent_id] == 9
+    """
+    def __init__(self, _net_dim=256, _state_dim=8, _action_dim=2, _learning_rate=1e-4,
+                 _if_per_or_gae=False, _env_num=1, _gpu_id=0):
+        AgentBase.__init__(self)
         self.ClassAct = Actor
         self.ClassCri = CriticTwin
         self.if_use_cri_target = True
@@ -573,33 +587,53 @@ class AgentTD3(AgentBase):
         self.policy_noise = 0.2  # standard deviation of policy noise
         self.update_freq = 2  # delay update frequency
 
-    def init(self, net_dim, state_dim, action_dim, learning_rate=1e-4, if_use_per=False, env_num=1, agent_id=0):
-        super().init(net_dim, state_dim, action_dim, learning_rate, if_use_per, env_num, agent_id)
-        if if_use_per:
-            self.criterion = torch.nn.SmoothL1Loss(reduction='none' if if_use_per else 'mean')
+    def init(self, net_dim=256, state_dim=8, action_dim=2,
+             learning_rate=1e-4, if_per_or_gae=False, env_num=1, gpu_id=0):
+        """
+        Explict call ``self.init()`` to overwrite the ``self.object`` in ``__init__()`` for multiprocessing. 
+        """
+        AgentBase.init(self, net_dim, state_dim, action_dim, learning_rate, if_per_or_gae, env_num, gpu_id)
+        if if_per_or_gae:  # if_use_per
+            self.criterion = torch.nn.SmoothL1Loss(reduction='none')
             self.get_obj_critic = self.get_obj_critic_per
         else:
-            self.criterion = torch.nn.SmoothL1Loss(reduction='none' if if_use_per else 'mean')
+            self.criterion = torch.nn.SmoothL1Loss(reduction='mean')
             self.get_obj_critic = self.get_obj_critic_raw
-
+            
     def update_net(self, buffer, batch_size, repeat_times, soft_update_tau) -> tuple:
+        """
+        Update the neural networks by sampling batch data from ``ReplayBuffer``.
+        
+        :param buffer[object]: the ReplayBuffer instance that stores the trajectories.
+        :param batch_size[int]: the size of batch data for Stochastic Gradient Descent (SGD).
+        :param repeat_times[float]: the re-using times of each trajectory.
+        :param soft_update_tau[float]: the soft update parameter.
+        :return: a tuple of the log information.
+        """
         buffer.update_now_len()
 
         obj_critic = None
         obj_actor = None
         for update_c in range(int(buffer.now_len / batch_size * repeat_times)):
             obj_critic, state = self.get_obj_critic(buffer, batch_size)
-            self.optim_update(self.cri_optim, obj_critic)
+            self.optim_update(self.cri_optim, obj_critic, self.cri.parameters())
 
             action_pg = self.act(state)  # policy gradient
             obj_actor = -self.cri_target(state, action_pg).mean()  # use cri_target instead of cri for stable training
-            self.optim_update(self.act_optim, obj_actor)
+            self.optim_update(self.act_optim, obj_actor, self.act.parameters())
             if update_c % self.update_freq == 0:  # delay update
                 self.soft_update(self.cri_target, self.cri, soft_update_tau)
                 self.soft_update(self.act_target, self.act, soft_update_tau)
         return obj_critic.item() / 2, obj_actor.item()
 
     def get_obj_critic_raw(self, buffer, batch_size):
+        """
+        Calculate the loss of networks with **uniform sampling**.
+        
+        :param buffer[object]: the ReplayBuffer instance that stores the trajectories.
+        :param batch_size[int]: the size of batch data for Stochastic Gradient Descent (SGD).
+        :return: the loss of the network and Q values.
+        """
         with torch.no_grad():
             reward, mask, action, state, next_s = buffer.sample_batch(batch_size)
             next_a = self.act_target.get_action(next_s, self.policy_noise)  # policy noise
@@ -610,8 +644,12 @@ class AgentTD3(AgentBase):
         return obj_critic, state
 
     def get_obj_critic_per(self, buffer, batch_size):
-        """Prioritized Experience Replay
-        Contributor: Github GyChou
+        """
+        Calculate the loss of the network with **Prioritized Experience Replay (PER)**.
+        
+        :param buffer[object]: the ReplayBuffer instance that stores the trajectories.
+        :param batch_size[int]: the size of batch data for Stochastic Gradient Descent (SGD).
+        :return: the loss of the network and Q values.
         """
         with torch.no_grad():
             reward, mask, action, state, next_s, is_weights = buffer.sample_batch(batch_size)
@@ -620,10 +658,10 @@ class AgentTD3(AgentBase):
             q_label = reward + mask * next_q
 
         q1, q2 = self.cri.get_q1_q2(state, action)
-        obj_critic = ((self.criterion(q1, q_label) + self.criterion(q2, q_label)) * is_weights).mean()
+        td_error = self.criterion(q1, q_label) + self.criterion(q2, q_label)
+        obj_critic = (td_error * is_weights).mean()
 
-        td_error = (q_label - torch.min(q1, q2).detach()).abs()
-        buffer.td_error_update(td_error)
+        buffer.td_error_update(td_error.detach())
         return obj_critic, state
 
 
