87f4c0a14577bc047ccc6364180c83085f7377d9
Steven Li <46941629+supersglzc@users.noreply.github.com>
2021-10-18 01:18:41 -0400

    Update net.py

--- elegantrl/net.py
+++ elegantrl/net.py
@@ -150,6 +150,13 @@ class QNetTwinDuel(nn.Module):  # D3QN: Dueling Double DQN
 
 
 class Actor(nn.Module):
+    """
+    A simple Actor class.
+    
+    :param mid_dim[int]: the middle dimension of networks
+    :param state_dim[int]: the dimension of state (the number of state vector)
+    :param action_dim[int]: the dimension of action (the number of discrete action)
+    """
     def __init__(self, mid_dim, state_dim, action_dim):
         super().__init__()
         self.net = nn.Sequential(nn.Linear(state_dim, mid_dim), nn.ReLU(),
@@ -158,9 +165,22 @@ class Actor(nn.Module):
                                  nn.Linear(mid_dim, action_dim))
 
     def forward(self, state):
+        """
+        The forward function.
+
+        :param state[np.array]: the input state.
+        :return: the output tensor.
+        """
         return self.net(state).tanh()  # action.tanh()
 
     def get_action(self, state, action_std):
+        """
+        The forward function with Gaussian noise.
+
+        :param state[np.array]: the input state.
+        :param action_std[float]: the standard deviation of the Gaussian distribution.
+        :return: the output tensor.
+        """
         action = self.net(state).tanh()
         noise = (torch.randn_like(action) * action_std).clamp(-0.5, 0.5)
         return (action + noise).clamp(-1.0, 1.0)
@@ -315,6 +335,13 @@ class ActorDiscretePPO(nn.Module):
 
 
 class Critic(nn.Module):
+    """
+    A simple Critic class.
+    
+    :param mid_dim[int]: the middle dimension of networks
+    :param state_dim[int]: the dimension of state (the number of state vector)
+    :param action_dim[int]: the dimension of action (the number of discrete action)
+    """
     def __init__(self, mid_dim, state_dim, action_dim):
         super().__init__()
         self.net = nn.Sequential(nn.Linear(state_dim + action_dim, mid_dim), nn.ReLU(),
@@ -323,6 +350,13 @@ class Critic(nn.Module):
                                  nn.Linear(mid_dim, 1))
 
     def forward(self, state, action):
+        """
+        The forward function.
+
+        :param state[np.array]: the input state.
+        :param state[float]: the input action.
+        :return: the output tensor.
+        """
         return self.net(torch.cat((state, action), dim=1))  # Q value
 
 
