2dd30ec7dd783544f6214e271170a089f8a7a3d0
Steven Li <46941629+supersglzc@users.noreply.github.com>
2021-10-18 21:48:50 -0400

    Update net.py

--- elegantrl/net.py
+++ elegantrl/net.py
@@ -361,6 +361,13 @@ class Critic(nn.Module):
 
 
 class CriticTwin(nn.Module):  # shared parameter
+    """
+    The Critic class for **Clipped Double DQN**.
+    
+    :param mid_dim[int]: the middle dimension of networks
+    :param state_dim[int]: the dimension of state (the number of state vector)
+    :param action_dim[int]: the dimension of action (the number of discrete action)
+    """
     def __init__(self, mid_dim, state_dim, action_dim, if_use_dn=False):
         super().__init__()
         if if_use_dn:
