2c526875d51d927dc42eceadc9c198b7270529c2
Steven Li <46941629+supersglzc@users.noreply.github.com>
2021-10-20 19:17:28 -0400

    Update agent.py

--- elegantrl/agent.py
+++ elegantrl/agent.py
@@ -138,7 +138,263 @@ class AgentBase:
                 load_torch_file(obj, save_path) if os.path.isfile(save_path) else None
 
 
+class AgentDQN(AgentBase):
+    """
+    Bases: ``elegantrl.agent.AgentBase``
+    
+    Deep Q-Network algorithm. “Human-Level Control Through Deep Reinforcement Learning”. Mnih V. et al.. 2015.
+    
+    :param net_dim[int]: the dimension of networks (the width of neural networks)
+    :param state_dim[int]: the dimension of state (the number of state vector)
+    :param action_dim[int]: the dimension of action (the number of discrete action)
+    :param learning_rate[float]: learning rate of optimizer
+    :param if_per_or_gae[bool]: PER (off-policy) or GAE (on-policy) for sparse reward
+    :param env_num[int]: the env number of VectorEnv. env_num == 1 means don't use VectorEnv
+    :param agent_id[int]: if the visible_gpu is '1,9,3,4', agent_id=1 means (1,9,4,3)[agent_id] == 9
+    """
+    def __init__(self, _net_dim=256, _state_dim=8, _action_dim=2, _learning_rate=1e-4,
+                 _if_per_or_gae=False, _env_num=1, _gpu_id=0):
+        AgentBase.__init__(self)
+        self.ClassCri = None  # self.ClassCri = QNetDuel if self.if_use_dueling else QNet
+        self.if_use_dueling = True  # self.ClassCri = QNetDuel if self.if_use_dueling else QNet
+        self.explore_rate = 0.25  # the probability of choosing action randomly in epsilon-greedy
 
+    def init(self, net_dim=256, state_dim=8, action_dim=2,
+             learning_rate=1e-4, if_per_or_gae=False, env_num=1, gpu_id=0):
+        """
+        Explict call ``self.init()`` to overwrite the ``self.object`` in ``__init__()`` for multiprocessing. 
+        """
+        self.ClassCri = QNetDuel if self.if_use_dueling else QNet
+        AgentBase.init(self, net_dim, state_dim, action_dim, learning_rate, if_per_or_gae, env_num, gpu_id)
+
+        if if_per_or_gae:  # if_use_per
+            self.criterion = torch.nn.SmoothL1Loss(reduction='none')
+            self.get_obj_critic = self.get_obj_critic_per
+        else:
+            self.criterion = torch.nn.SmoothL1Loss(reduction='mean')
+            self.get_obj_critic = self.get_obj_critic_raw
+
+    def select_actions(self, states) -> np.ndarray:  # for discrete action space
+        """
+        Select discrete actions given an array of states.
+        
+        .. note::
+            Using ϵ-greedy to uniformly random select actions for randomness.
+        
+        :param states[np.ndarray]: an array of states in a shape (batch_size, state_dim, ).
+        :return: an array of actions in a shape (batch_size, action_dim, ) where each action is clipped into range(-1, 1).
+        """
+        if rd.rand() < self.explore_rate:  # epsilon-greedy
+            a_int = torch.randint(self.action_dim, size=state.shape[0])  # choosing action randomly
+        else:
+            action = self.act(state.to(self.device))
+            a_int = action.argmax(dim=1)
+        return a_int.detach().cpu()
+
+    def explore_one_env(self, env, target_step) -> list:
+        """
+        Collect trajectories through the actor-environment interaction for a **single** environment instance.
+        
+        :param env[object]: the DRL environment instance.
+        :param target_step[int]: the total step for the interaction.
+        :return: a list of trajectories [traj, ...] where each trajectory is a list of transitions [(state, other), ...].
+        """
+        traj = list()
+        state = self.states[0]
+        for _ in range(target_step):
+            ten_state = torch.as_tensor(state, dtype=torch.float32)
+            ten_action = self.select_actions(ten_state.unsqueeze(0))[0]
+            action = ten_action.numpy()  # isinstance(action, int)
+            next_s, reward, done, _ = env.step(action)
+
+            ten_other = torch.empty(2 + 1)
+            ten_other[0] = reward
+            ten_other[1] = done
+            ten_other[2] = ten_action
+            traj.append((ten_state, ten_other))
+
+            state = env.reset() if done else next_s
+        self.states[0] = state
+
+        traj_state = torch.stack([item[0] for item in traj])
+        traj_other = torch.stack([item[1] for item in traj])
+        traj_list = [(traj_state, traj_other), ]
+        return self.convert_trajectory(traj_list, reward_scale, gamma)  # [traj_env_0, ...]
+    
+    def explore_vec_env(self, env, target_step) -> list:
+        """
+        Collect trajectories through the actor-environment interaction for a **vectorized** environment instance.
+        
+        :param env[object]: the DRL environment instance.
+        :param target_step[int]: the total step for the interaction.
+        :return: a list of trajectories [traj, ...] where each trajectory is a list of transitions [(state, other), ...].
+        """
+        ten_states = self.states
+
+        traj = list()
+        for _ in range(target_step):
+            ten_actions = self.select_actions(ten_states)
+            ten_next_states, ten_rewards, ten_dones = env.step(ten_actions)
+
+            ten_others = torch.cat((ten_rewards.unsqueeze(0),
+                                    ten_dones.unsqueeze(0),
+                                    ten_actions.unsqueeze(0)))
+            traj.append((ten_states, ten_others))
+            ten_states = ten_next_states
+
+        self.states = ten_states
+
+        traj_state = torch.stack([item[0] for item in traj])
+        traj_other = torch.stack([item[1] for item in traj])
+        traj_list = [(traj_state[:, env_i, :], traj_other[:, env_i, :])
+                     for env_i in range(len(self.states))]
+        return self.convert_trajectory(traj_list, reward_scale, gamma)  # [traj_env_0, ...]
+
+    def update_net(self, buffer, batch_size, repeat_times, soft_update_tau) -> tuple:
+        """
+        Update the neural networks by sampling batch data from ``ReplayBuffer``.
+        
+        :param buffer[object]: the ReplayBuffer instance that stores the trajectories.
+        :param batch_size[int]: the size of batch data for Stochastic Gradient Descent (SGD).
+        :param repeat_times[float]: the re-using times of each trajectory.
+        :param soft_update_tau[float]: the soft update parameter.
+        :return: a tuple of the log information.
+        """
+        buffer.update_now_len()
+        obj_critic = q_value = None
+        for _ in range(int(buffer.now_len / batch_size * repeat_times)):
+            obj_critic, q_value = self.get_obj_critic(buffer, batch_size)
+            self.optim_update(self.cri_optim, obj_critic, self.cri.parameters())
+            self.soft_update(self.cri_target, self.cri, soft_update_tau)
+        return obj_critic.item(), q_value.mean().item()
+
+    def get_obj_critic_raw(self, buffer, batch_size):
+        """
+        Calculate the loss of the network and predict Q values with **uniform sampling**.
+        
+        :param buffer[object]: the ReplayBuffer instance that stores the trajectories.
+        :param batch_size[int]: the size of batch data for Stochastic Gradient Descent (SGD).
+        :return: the loss of the network and Q values.
+        """
+        with torch.no_grad():
+            reward, mask, action, state, next_s = buffer.sample_batch(batch_size)
+            next_q = self.cri_target(next_s).max(dim=1, keepdim=True)[0]
+            q_label = reward + mask * next_q
+
+        q_value = self.cri(state).gather(1, action.long())
+        obj_critic = self.criterion(q_value, q_label)
+        return obj_critic, q_value
+    
+    def get_obj_critic_per(self, buffer, batch_size):
+        """
+        Calculate the loss of the network and predict Q values with **Prioritized Experience Replay (PER)**.
+        
+        :param buffer[object]: the ReplayBuffer instance that stores the trajectories.
+        :param batch_size[int]: the size of batch data for Stochastic Gradient Descent (SGD).
+        :return: the loss of the network and Q values.
+        """
+        with torch.no_grad():
+            reward, mask, action, state, next_s, is_weights = buffer.sample_batch(batch_size)
+            next_q = self.cri_target(next_s).max(dim=1, keepdim=True)[0]
+            q_label = reward + mask * next_q
+
+        q_value = self.cri(state).gather(1, action.long())
+        td_error = self.criterion(q_value, q_label)  # or td_error = (q_value - q_label).abs()
+        obj_critic = (td_error * is_weights).mean()
+
+        buffer.td_error_update(td_error.detach())
+        return obj_critic, q_value
+
+ 
+class AgentDoubleDQN(AgentDQN):
+    """
+    Bases: ``elegantrl.agent.AgentDQN``
+    
+    Double Deep Q-Network algorithm. “Deep Reinforcement Learning with Double Q-learning”. H. V. Hasselt et al.. 2015.
+    
+    :param net_dim[int]: the dimension of networks (the width of neural networks)
+    :param state_dim[int]: the dimension of state (the number of state vector)
+    :param action_dim[int]: the dimension of action (the number of discrete action)
+    :param learning_rate[float]: learning rate of optimizer
+    :param if_per_or_gae[bool]: PER (off-policy) or GAE (on-policy) for sparse reward
+    :param env_num[int]: the env number of VectorEnv. env_num == 1 means don't use VectorEnv
+    :param agent_id[int]: if the visible_gpu is '1,9,3,4', agent_id=1 means (1,9,4,3)[agent_id] == 9
+    """
+    def __init__(self, _net_dim=256, _state_dim=8, _action_dim=2, _learning_rate=1e-4,
+                 _if_per_or_gae=False, _env_num=1, _gpu_id=0):
+        AgentDQN.__init__(self)
+        self.soft_max = torch.nn.Softmax(dim=1)
+        
+    def init(self, net_dim=256, state_dim=8, action_dim=2,
+             learning_rate=1e-4, if_per_or_gae=False, env_num=1, gpu_id=0):
+        """
+        Explict call ``self.init()`` to overwrite the ``self.object`` in ``__init__()`` for multiprocessing. 
+        """
+        self.ClassCri = QNetTwinDuel if self.if_use_dueling else QNetTwin
+        AgentBase.init(self, net_dim, state_dim, action_dim, learning_rate, if_per_or_gae, env_num, gpu_id)
+
+        if if_per_or_gae:  # if_use_per
+            self.criterion = torch.nn.SmoothL1Loss(reduction='none')
+            self.get_obj_critic = self.get_obj_critic_per
+        else:
+            self.criterion = torch.nn.SmoothL1Loss(reduction='mean')
+            self.get_obj_critic = self.get_obj_critic_raw
+
+    def select_actions(self, states) -> np.ndarray:  # for discrete action space
+        """
+        Select discrete actions given an array of states.
+        
+        .. note::
+            Using softmax to random select actions with proportional probabilities for randomness.
+        
+        :param states[np.ndarray]: an array of states in a shape (batch_size, state_dim, ).
+        :return: an array of actions in a shape (batch_size, action_dim, ) where each action is clipped into range(-1, 1).
+        """
+        action = self.act(state.to(self.device))
+        if rd.rand() < self.explore_rate:  # epsilon-greedy
+            a_prob = self.soft_max(action)
+            a_int = torch.multinomial(a_prob, num_samples=1, replacement=True)[:, 0]
+            # a_int = rd.choice(self.action_dim, prob=a_prob)  # numpy version
+        else:
+            a_int = action.argmax(dim=1)
+        return a_int.detach().cpu()
+
+    def get_obj_critic_raw(self, buffer, batch_size) -> (torch.Tensor, torch.Tensor):
+        """
+        Calculate the loss of the network and predict Q values with **uniform sampling**.
+        
+        :param buffer[object]: the ReplayBuffer instance that stores the trajectories.
+        :param batch_size[int]: the size of batch data for Stochastic Gradient Descent (SGD).
+        :return: the loss of the network and Q values.
+        """
+        with torch.no_grad():
+            reward, mask, action, state, next_s = buffer.sample_batch(batch_size)
+            next_q = torch.min(*self.cri_target.get_q1_q2(next_s)).max(dim=1, keepdim=True)[0]
+            q_label = reward + mask * next_q
+
+        q1, q2 = [qs.gather(1, action.long()) for qs in self.act.get_q1_q2(state)]
+        obj_critic = self.criterion(q1, q_label) + self.criterion(q2, q_label)
+        return obj_critic, q1
+
+    def get_obj_critic_per(self, buffer, batch_size):
+        """
+        Calculate the loss of the network and predict Q values with **Prioritized Experience Replay (PER)**.
+        
+        :param buffer[object]: the ReplayBuffer instance that stores the trajectories.
+        :param batch_size[int]: the size of batch data for Stochastic Gradient Descent (SGD).
+        :return: the loss of the network and Q values.
+        """
+        with torch.no_grad():
+            reward, mask, action, state, next_s, is_weights = buffer.sample_batch(batch_size)
+            next_q = torch.min(*self.cri_target.get_q1_q2(next_s)).max(dim=1, keepdim=True)[0]
+            q_label = reward + mask * next_q
+
+        q1, q2 = [qs.gather(1, action.long()) for qs in self.act.get_q1_q2(state)]
+        td_error = self.criterion(q1, q_label) + self.criterion(q2, q_label)
+        obj_critic = (td_error * is_weights).mean()
+
+        buffer.td_error_update(td_error.detach())
+        return obj_critic, q1
 
 
 '''Actor-Critic Methods (Policy Gradient)'''
