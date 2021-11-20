fcd45ba4b59c3611f4e4577707da7a51f11e47e1
Steven <zl2993@columbia.edu>
2021-11-20 14:59:35 -0500

    new demo

--- elegantrl/tutorial_Pendulum.py
+++ elegantrl/tutorial_Pendulum.py
@@ -1,19 +1,20 @@
 from elegantrl.train.run_tutorial import *
 from elegantrl.train.config import Arguments
-from elegantrl.agents.AgentDoubleDQN import AgentDoubleDQN
+from elegantrl.agents.AgentPPO import AgentPPO
 from elegantrl.envs.Gym import build_env
 import gym
 
 gym.logger.set_level(40)  # Block warning
 
-agent = AgentDoubleDQN()
-env = build_env('CartPole-v0')
+agent = AgentPPO()
+env = build_env('Pendulum-v1')
 args = Arguments(env, agent)
 
-args.target_return = 195
-
-args.reward_scale = 2 ** -1
-args.target_step = args.env.max_step * 4
+args.gamma = 0.97
+args.net_dim = 2 ** 8
+args.worker_num = 2
+args.reward_scale = 2 ** -2
+args.target_step = 200 * 16  # max_step = 200
 
 args.eval_gap = 2 ** 5
 
