edce5e34d07dd6021ca483fa1cfb546f4dfe2453
shixun404 <shixun404@gmail.com>
2021-11-21 20:06:24 -0500

    mixer net

--- elegantrl/agents/net.py
+++ elegantrl/agents/net.py
@@ -1088,3 +1088,22 @@ class ActorSimplify:
         action = self.actor_net(states)[0]
         return action.detach().cpu().numpy()
 
+class RNNAgent(nn.Module):
+    def __init__(self, input_shape, args):
+        super(RNNAgent, self).__init__()
+        self.args = args
+
+        self.fc1 = torch.nn.Linear(input_shape, args.rnn_hidden_dim)
+        self.rnn = torch.nn.GRUCell(args.rnn_hidden_dim, args.rnn_hidden_dim)
+        self.fc2 = torch.nn.Linear(args.rnn_hidden_dim, args.n_actions)
+
+    def init_hidden(self):
+        # make hidden states on same device as model
+        return self.fc1.weight.new(1, self.args.rnn_hidden_dim).zero_()
+
+    def forward(self, inputs, hidden_state):
+        x = torch.nn.F.relu(self.fc1(inputs))
+        h_in = hidden_state.reshape(-1, self.args.rnn_hidden_dim)
+        h = self.rnn(x, h_in)
+        q = self.fc2(h)
+        return q, h
