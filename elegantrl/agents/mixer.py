be9afcc925f1ddd9168c9c808179e1c1ca047d11
shixun404 <shixun404@gmail.com>
2021-12-02 17:29:06 -0500

    update

--- elegantrl/agents/mixer.py
+++ elegantrl/agents/mixer.py
@@ -5,7 +5,77 @@ import numpy as np
 import torch as th
 import torch.nn as nn
 
+import torch as th
+import torch.nn as nn
+import torch.nn.functional as F
+import numpy as np
 
+class QTran(nn.Module):
+    def __init__(self, args):
+        super(QTran, self).__init__()
+
+        self.args = args
+
+        self.n_agents = args.n_agents
+        self.n_actions = args.n_actions
+        self.state_dim = int(np.prod(args.state_shape))
+
+        self.embed_dim = args.mixing_embed_dim
+
+        q_input_size = self.state_dim + self.args.rnn_hidden_dim + self.n_actions
+
+        self.Q = nn.Sequential(nn.Linear(q_input_size, self.embed_dim),
+                                nn.ReLU(),
+                                nn.Linear(self.embed_dim, self.embed_dim),
+                                nn.ReLU(),
+                                nn.Linear(self.embed_dim, 1))
+
+        # V(s)
+        self.V = nn.Sequential(nn.Linear(self.state_dim, self.embed_dim),
+                                nn.ReLU(),
+                                nn.Linear(self.embed_dim, self.embed_dim),
+                                nn.ReLU(),
+                                nn.Linear(self.embed_dim, 1))
+        ae_input = self.args.rnn_hidden_dim + self.n_actions
+        self.action_encoding = nn.Sequential(nn.Linear(ae_input, ae_input),
+                                                nn.ReLU(),
+                                                nn.Linear(ae_input, ae_input))
+
+    def forward(self, batch, hidden_states, actions=None):
+        bs = batch.batch_size
+        ts = batch.max_seq_length
+
+        states = batch["state"].reshape(bs * ts, self.state_dim)
+
+        if self.arch == "coma_critic":
+            if actions is None:
+                # Use the actions taken by the agents
+                actions = batch["actions_onehot"].reshape(bs * ts, self.n_agents * self.n_actions)
+            else:
+                # It will arrive as (bs, ts, agents, actions), we need to reshape it
+                actions = actions.reshape(bs * ts, self.n_agents * self.n_actions)
+            inputs = th.cat([states, actions], dim=1)
+        elif self.arch == "qtran_paper":
+            if actions is None:
+                # Use the actions taken by the agents
+                actions = batch["actions_onehot"].reshape(bs * ts, self.n_agents, self.n_actions)
+            else:
+                # It will arrive as (bs, ts, agents, actions), we need to reshape it
+                actions = actions.reshape(bs * ts, self.n_agents, self.n_actions)
+
+            hidden_states = hidden_states.reshape(bs * ts, self.n_agents, -1)
+            agent_state_action_input = th.cat([hidden_states, actions], dim=2)
+            agent_state_action_encoding = self.action_encoding(agent_state_action_input.reshape(bs * ts * self.n_agents, -1)).reshape(bs * ts, self.n_agents, -1)
+            agent_state_action_encoding = agent_state_action_encoding.sum(dim=1) # Sum across agents
+
+            inputs = th.cat([states, agent_state_action_encoding], dim=1)
+
+        q_outputs = self.Q(inputs)
+
+        states = batch["state"].reshape(bs * ts, self.state_dim)
+        v_outputs = self.V(states)
+
+        return q_outputs, v_outputs
 class VDNMixer(nn.Module):
     def __init__(self):
         super(VDNMixer, self).__init__()
