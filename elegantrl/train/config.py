48cf85962d6b8317a87cd7c18b271884e0f9c71e
Steven <zl2993@columbia.edu>
2021-12-23 16:03:38 -0500

    config docs

--- elegantrl/train/config.py
+++ elegantrl/train/config.py
@@ -4,109 +4,109 @@ import numpy as np
 
 
 class Arguments:  # [ElegantRL.2021.10.21]
+    """
+        Configuration map.
+
+        :param env[object]: the environment object in ElegantRL.
+        :param agent[object]: the agent object in ElegantRL.
+
+        **Attributes for environment setup**
+
+        Attributes
+        ----------------
+            env : object
+                environment object in ElegantRL.
+            env_num : int
+                number of sub-environments. For VecEnv, env_num > 1.
+            max_step : int
+                max step of an episode.
+            state_dim : int
+                state dimension of the environment.
+            action_dim : int
+                action dimension of the environment.
+            if_discrete : boolean
+                discrete or continuous action space.
+            target_return : float
+                target average episodic return.
+
+        **Attributes for model training**
+
+        Attributes
+        ----------------
+            agent : object
+                agent object in ElegantRL.
+            if_off_policy : boolean
+                off-policy or on-policy for the DRL algorithm.
+            net_dim : int
+                neural network width.
+            max_memo : int
+                capacity of replay buffer.
+            batch_size : int
+                number of transitions sampled in one iteration.
+            target_step : int
+                repeatedly update network to keep critic's loss small.
+            repeat_times : int
+                collect target_step, then update network.
+            break_step : int
+                break training after total_step > break_step.
+            if_allow_break : boolean
+                allow break training when reach goal (early termination).
+            if_per_or_gae : boolean
+                use Prioritized Experience Replay (PER) or not for off-policy algorithms.
+
+                use Generalized Advantage Estimation or not for on-policy algorithms.
+            gamma : float
+                discount factor of future rewards.
+            reward_scale : int
+                an approximate target reward.
+            learning_rate : float
+                the learning rate.
+            soft_update_tau : float
+                soft update parameter for target networks.
+
+        **Attributes for model evaluation**
+
+        Attributes
+        ----------------
+            eval_env : object
+                environment object for model evaluation.
+            eval_gap : int
+                time gap for periodical evaluation (in seconds).
+            eval_times1 : int
+                number of times that get episode return in first.
+            eval_times2 : int
+                number of times that get episode return in second.
+            eval_gpu_id : int or None
+                the GPU id for the evaluation environment.
+
+                -1 means use cpu, >=0 means use GPU, None means set as learner_gpus[0].
+            if_overwrite : boolean
+                save policy networks with different episodic return separately or overwrite.
+
+        **Attributes for resource allocation**
+
+        Attributes
+        ----------------
+            worker_num : int
+                rollout workers number per GPU (adjust it to get high GPU usage).
+            thread_num : int
+                cpu_num for evaluate model.
+            random_seed : int
+                initialize random seed in ``init_before_training``.
+            learner_gpus : list
+                GPU ids for learner.
+            workers_gpus : list
+                GPU ids for worker.
+            ensemble_gpus : list
+                GPU ids for population-based training (PBT).
+            ensemble_gap : list
+                time gap for leaderboard update in tournament-based ensemble training.
+            cwd : string
+                directory path to save the model.
+            if_remove : boolean
+                remove the cwd folder? (True, False, None:ask me).
+    """
     def __init__(self, env, agent):
-        """
-            Configuration map.
-
-            :param env[object]: the environment object in ElegantRL.
-            :param agent[object]: the agent object in ElegantRL.
-
-            **Attributes for environment setup**
-
-            Attributes
-            ----------------
-                env : object
-                    environment object in ElegantRL.
-                env_num : int
-                    number of sub-environments. For VecEnv, env_num > 1.
-                max_step : int
-                    max step of an episode.
-                state_dim : int
-                    state dimension of the environment.
-                action_dim : int
-                    action dimension of the environment.
-                if_discrete : boolean
-                    discrete or continuous action space.
-                target_return : float
-                    target average episodic return.
-
-            **Attributes for model training**
-
-            Attributes
-            ----------------
-                agent : object
-                    agent object in ElegantRL.
-                if_off_policy : boolean
-                    off-policy or on-policy for the DRL algorithm.
-                net_dim : int
-                    neural network width.
-                max_memo : int
-                    capacity of replay buffer.
-                batch_size : int
-                    number of transitions sampled in one iteration.
-                target_step : int
-                    repeatedly update network to keep critic's loss small.
-                repeat_times : int
-                    collect target_step, then update network.
-                break_step : int
-                    break training after total_step > break_step.
-                if_allow_break : boolean
-                    allow break training when reach goal (early termination).
-                if_per_or_gae : boolean
-                    use Prioritized Experience Replay (PER) or not for off-policy algorithms.
-
-                    use Generalized Advantage Estimation or not for on-policy algorithms.
-                gamma : float
-                    discount factor of future rewards.
-                reward_scale : int
-                    an approximate target reward.
-                learning_rate : float
-                    the learning rate.
-                soft_update_tau : float
-                    soft update parameter for target networks.
-
-            **Attributes for model evaluation**
-
-            Attributes
-            ----------------
-                eval_env : object
-                    environment object for model evaluation.
-                eval_gap : int
-                    time gap for periodical evaluation (in seconds).
-                eval_times1 : int
-                    number of times that get episode return in first.
-                eval_times2 : int
-                    number of times that get episode return in second.
-                eval_gpu_id : int or None
-                    the GPU id for the evaluation environment.
-
-                    -1 means use cpu, >=0 means use GPU, None means set as learner_gpus[0].
-                if_overwrite : boolean
-                    save policy networks with different episodic return separately or overwrite.
-
-            **Attributes for resource allocation**
-
-            Attributes
-            ----------------
-                worker_num : int
-                    rollout workers number per GPU (adjust it to get high GPU usage).
-                thread_num : int
-                    cpu_num for evaluate model.
-                random_seed : int
-                    initialize random seed in ``init_before_training``.
-                learner_gpus : list
-                    GPU ids for learner.
-                workers_gpus : list
-                    GPU ids for worker.
-                ensemble_gpus : list
-                    GPU ids for population-based training (PBT).
-                ensemble_gap : list
-                    time gap for leaderboard update in tournament-based ensemble training.
-                cwd : string
-                    directory path to save the model.
-                if_remove : boolean
-                    remove the cwd folder? (True, False, None:ask me).
-        """
         self.env = env  # the environment for training
         self.env_num = getattr(env, 'env_num', 1)  # env_num = 1. In vector env, env_num > 1.
 
