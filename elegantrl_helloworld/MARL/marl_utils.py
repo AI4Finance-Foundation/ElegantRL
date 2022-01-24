35d846491ba28f9fe3b2792590bf567ad834126c
Markus <mysecondworld@firesurf.de>
2022-01-24 19:21:11 +0100

    Inline variable that is immediately returned

--- elegantrl_helloworld/MARL/marl_utils.py
+++ elegantrl_helloworld/MARL/marl_utils.py
@@ -235,8 +235,9 @@ class BasicMAC:
         # Only select actions for the selected batch elements in bs
         avail_actions = ep_batch["avail_actions"][:, t_ep]
         agent_outputs = self.forward(ep_batch, t_ep, test_mode=test_mode)
-        chosen_actions = self.action_selector.select_action(agent_outputs[bs], avail_actions[bs], t_env, test_mode=test_mode)
-        return chosen_actions
+        return self.action_selector.select_action(
+            agent_outputs[bs], avail_actions[bs], t_env, test_mode=test_mode
+        ) # choosen action
 
     def forward(self, ep_batch, t, test_mode=False):
         agent_inputs = self._build_inputs(ep_batch, t)
