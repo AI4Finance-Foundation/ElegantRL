440c044e9490dd55c8d30d2d9b48ba49f83ec690
Momin Haider <h@momin.dev>
2022-02-01 16:34:21 -0500

    Added a script which lists all environments
    - It can be tricky to remember the names of each environment one might
    want to train on (e.g. LunarLanderContinuous, BipedalWalkerHardcore), so
    I added this script which simply lists out all OpenAI gym environments.

---
+++ examples/list_gym_envs.py
@@ -0,0 +1,10 @@
+# This script lists out all OpenAI gym environments that can be tested on. Some of them
+# (Ant, Hopper, etc.) require additional external dependencies (mujoco_py, etc.).
+
+from gym import envs
+
+all_envs = envs.registry.all()
+env_ids = [env_spec.id for env_spec in all_envs]
+
+for env_id in env_ids:
+    print(env_id)
