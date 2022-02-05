52946b61946032a96fde950bbaa41edc6df51e62
Momin Haider <h@momin.dev>
2022-02-05 14:12:50 -0500

    Switch Hopper tutorial back to PPO
    PPO is behaving with much more stability than ModSAC, so I'm reverting
    this back.

--- examples/tutorial_Hopper-v3.py
+++ examples/tutorial_Hopper-v3.py
@@ -1,5 +1,5 @@
 import gym
-from elegantrl.agent import AgentModSAC
+from elegantrl.agent import AgentPPO
 from elegantrl.config import get_gym_env_args, Arguments
 from elegantrl.run import *
 
@@ -8,10 +8,13 @@ env_name = 'Hopper-v3'
 
 # retrieve appropriate training arguments for this environment
 env_args = get_gym_env_args(gym.make(env_name), if_print=False)
-args = Arguments(AgentModSAC, env_func=gym.make, env_args=env_args)
+args = Arguments(AgentPPO, env_func=gym.make, env_args=env_args)
 
 # set/modify any arguments you'd like to here
+args.batch_size = 2 ** 16
 args.eval_times = 2 ** 4
+args.max_memo = 2 ** 16
+args.target_step = 2 ** 16
 
 # print out arguments in an easy-to-read format to show you what you're about to train...
 args.print()
