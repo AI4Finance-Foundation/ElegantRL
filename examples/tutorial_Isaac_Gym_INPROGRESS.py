bf6f2a907134455db1115a26beaef6a15dac09c6
Momin Haider <h@momin.dev>
2022-02-15 16:00:00 -0500

    Added tutorial for training with Isaac Gym
    I've added a tutorial (closely modeled off the docs) to train on a
    vectorized environment with Isaac Gym. It should be noted that it
    currently runs into a few errors, but this gives us a baseline to work
    with and improve upon.

---
+++ examples/tutorial_Isaac_Gym_INPROGRESS.py
@@ -0,0 +1,41 @@
+from elegantrl.agent import AgentPPO
+from elegantrl.config import Arguments
+from elegantrl.envs.IsaacGym import IsaacVecEnv, IsaacOneEnv
+from elegantrl.run import train_and_evaluate_mp
+
+env_func = IsaacVecEnv
+env_args = {
+    "env_num": 4096,
+    "env_name": "Ant",
+    "max_step": 1000,
+    "state_dim": 60,
+    "action_dim": 8,
+    "if_discrete": False,
+    "target_return": 14000.0,
+    "device_id": 0,  # set by worker
+    "if_print": False,  # if_print=False in default
+}
+
+args = Arguments(agent=AgentPPO, env_func=env_func, env_args=env_args)
+
+"""set one env for evaluator"""
+args.eval_env_func = IsaacOneEnv
+args.eval_env_args = args.env_args.copy()
+args.eval_env_args["env_num"] = 1
+
+"""set other hyper-parameters"""
+args.net_dim = 2**9
+args.batch_size = args.net_dim * 4
+args.target_step = args.max_step
+args.repeat_times = 2**4
+
+args.save_gap = 2**9
+args.eval_gap = 2**8
+args.eval_times1 = 2**0
+args.eval_times2 = 2**2
+
+args.worker_num = 1
+args.learner_gpus = 0
+
+if __name__ == '__main__':
+    train_and_evaluate_mp(args)
