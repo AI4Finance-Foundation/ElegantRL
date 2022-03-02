c2bfec97694798369dd6e8a390ca96f199841d97
Momin Haider <h@momin.dev>
2022-03-02 11:14:49 -0500

    Improving Function Documentation
    I've added many docstrings to the functions relevant to Isaac Gym and
    cleaned up some of the files.

--- elegantrl/envs/IsaacGym.py
+++ elegantrl/envs/IsaacGym.py
@@ -4,7 +4,7 @@ import torch
 from elegantrl.envs.isaac_tasks import isaacgym_task_map
 from elegantrl.envs.isaac_tasks.base.vec_task import VecTask
 from elegantrl.envs.utils.utils import set_seed
-from elegantrl.envs.utils.config_utils import load_task_config
+from elegantrl.envs.utils.config_utils import load_task_config, get_max_step_from_config
 from pprint import pprint
 from typing import Dict, Tuple
 
@@ -19,10 +19,22 @@ class IsaacVecEnv:
         headless=False,
         should_print=False,
     ):
-        """
-        Preprocesses an Isaac Gym environment for RL training.
-        [Isaac Gym](https://developer.nvidia.com/isaac-gym)
-        NVIDIA Isaac Gym - Preview 3 Release
+        """Preprocesses a vectorized Isaac Gym environment for RL training.
+        [Isaac Gym - Preview 3 Release](https://developer.nvidia.com/isaac-gym)
+        
+        Args:
+            env_name (str): the name of the environment to be processed.
+            env_num (int, optional): the number of environments to simulate on the
+                device. Defaults to whatever is specified in the corresponding config
+                file.
+            sim_device_id (int, optional): the GPU device id to render physics on.
+                Defaults to 0.
+            rl_device_id (int, optional): the GPU device id to perform RL training on.
+                Defaults to 0.
+            headless (bool, optional): whether or not the Isaac Gym environment should
+                render on-screen. Defaults to False.
+            should_print (bool, optional): whether or not the arguments should be
+                printed. Defaults to False.
         """
         task_config = load_task_config(env_name)
         sim_device = f"cuda:{sim_device_id}" if sim_device_id >= 0 else "cpu"
@@ -47,10 +59,12 @@ class IsaacVecEnv:
             assert all(env.action_space.high == np.ones(action_dimension))
             assert all(-env.action_space.low == np.ones(action_dimension))
 
-        # FIXME: figure out a better way to determine these
+        # FIXME: figure out a better way to determine this
         target_return = 10**10
-        max_step = 1000
-
+        
+        env_config = task_config["env"]
+        max_step = get_max_step_from_config(env_config)
+        
         self.device = torch.device(rl_device_id)
         self.env = env
         self.env_num = env.num_envs
@@ -77,13 +91,18 @@ class IsaacVecEnv:
         """Overrides the default number of environments if it's passed in.
 
         Args:
-            num_envs (int): new number of environments
-            config_args (Dict): configuration retrieved
+            num_envs (int): new number of environments.
+            config_args (Dict): configuration retrieved.
         """
         if num_envs > 0:
             config_args["env"]["numEnvs"] = num_envs
 
     def reset(self) -> torch.Tensor:
+        """Resets the environments in the VecTask that need to be reset.
+
+        Returns:
+            torch.Tensor: the next states in the simulation.
+        """
         states = self.env.reset()
         if isinstance(states, Dict):
             states = states["obs"]
@@ -93,6 +112,16 @@ class IsaacVecEnv:
     def step(
         self, actions: torch.Tensor
     ) -> Tuple[torch.Tensor, torch.Tensor, torch.Tensor, Dict]:
+        """Steps through the vectorized environment.
+
+        Args:
+            actions (torch.Tensor): a multidimensional tensor of actions to perform on
+                *each* environment.
+
+        Returns:
+            Tuple[torch.Tensor, torch.Tensor, torch.Tensor, Dict]: a tuple containing
+                observations, rewards, dones, and extra info.
+        """
         (observations_dict, rewards, dones, info_dict) = self.env.step(actions)
         observations = observations_dict["obs"]
         return (observations, rewards, dones, info_dict)
@@ -100,10 +129,17 @@ class IsaacVecEnv:
 
 class IsaacOneEnv(IsaacVecEnv):
     def __init__(self, env_name: str, device_id=0, headless=False, should_print=False):
-        """
-        Preprocess an Isaac Gym single environment for RL evaluating.
-        [Isaac Gym](https://developer.nvidia.com/isaac-gym)
-        NVIDIA Isaac Gym - Preview 3 Release
+        """Preprocesses a single Isaac Gym environment for RL evaluating.
+        [Isaac Gym - Preview 3 Release](https://developer.nvidia.com/isaac-gym)
+
+        Args:
+            env_name (str): the name of the environment to be processed.
+            device_id (int, optional): the GPU device id to render physics and perform
+                RL training. Defaults to 0.
+            headless (bool, optional): whether or not the Isaac Gym environment should
+                render on-screen. Defaults to False.
+            should_print (bool, optional): whether or not the arguments should be
+                printed. Defaults to False.
         """
         super().__init__(
             env_name=env_name,
@@ -115,6 +151,12 @@ class IsaacOneEnv(IsaacVecEnv):
         )
 
     def reset(self) -> np.ndarray:
+        """Resets the environments in the VecTask that need to be reset.
+
+        Returns:
+            np.ndarray: a numpy array containing the new state of the single
+                environment.
+        """
         tensor_state_dict = self.env.reset()
         tensor_states = tensor_state_dict["obs"]
         first_state = tensor_states[0]
@@ -123,6 +165,16 @@ class IsaacOneEnv(IsaacVecEnv):
     def step(
         self, action: np.ndarray
     ) -> Tuple[np.ndarray, np.ndarray, np.ndarray, Dict]:
+        """Steps through the single environment.
+
+        Args:
+            action (np.ndarray): a (possibly multidimensional) numpy array of actions
+                to perform on the single environment.
+
+        Returns:
+            Tuple[np.ndarray, np.ndarray, np.ndarray, Dict]: a tuple containing
+                observations, rewards, dones, and extra info.
+        """
         tensor_action = torch.as_tensor(action, dtype=torch.float32).unsqueeze(0)
         tensor_state_dict, tensor_reward, tensor_done, info_dict = self.env.step(
             tensor_action
--- examples/tutorial_Hopper-v3.py
+++ examples/tutorial_Hopper-v3.py
@@ -3,7 +3,8 @@ from elegantrl.agent import AgentPPO
 from elegantrl.config import get_gym_env_args, Arguments
 from elegantrl.run import *
 
-# set environment name here (e.g. 'Hopper-v3', 'LunarLanderContinuous-v2', 'BipedalWalker-v3')
+# set environment name here (e.g. 'Hopper-v3', 'LunarLanderContinuous-v2',
+# 'BipedalWalker-v3')
 env_name = "Hopper-v3"
 
 # retrieve appropriate training arguments for this environment
@@ -16,7 +17,8 @@ args.eval_times = 2**4
 args.max_memo = 2**16
 args.target_step = 2**16
 
-# print out arguments in an easy-to-read format to show you what you're about to train...
+# print out arguments in an easy-to-read format to show you what you're about to
+# train...
 args.print()
 
 # ...and go!
