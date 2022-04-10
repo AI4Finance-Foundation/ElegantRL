3637e94802e39dbd4b0060e76e643f19c47c96e4
YonV1943 曾伊言 <37322666+Yonv1943@users.noreply.github.com>
2022-04-10 11:16:33 +0800

    simple helloworld

--- elegantrl_helloworld/env.py
+++ elegantrl_helloworld/env.py
@@ -1,106 +1,95 @@
-from copy import deepcopy
-
 import gym
+import numpy as np
 
-gym.logger.set_level(40)  # Block warning
+
+class PendulumEnv(gym.Wrapper):  # a demo of custom gym env
+    def __init__(self, gym_env_name="Pendulum-v1"):
+        gym.logger.set_level(40)  # Block warning
+        super().__init__(env=gym.make(gym_env_name))
+
+        self.env_name = gym_env_name  # the name of this env.
+        self.max_step = getattr(self.env, '_max_episode_steps', None)  # the max step of an episode
+        self.state_dim = self.observation_space.shape[0]  # feature number of state
+        self.action_dim = self.action_space.shape[0]  # feature number of action
+        self.if_discrete = False  # discrete action or continuous action
+
+    def reset(self):
+        return self.env.reset().astype(np.float32)
+
+    def step(self, action: np.ndarray):
+        # OpenAI Pendulum env set its action space as (-2, +2). It is bad.
+        # We suggest that adjust action space to (-1, +1) when designing your own env.
+        state, reward, done, info_dict = self.env.step(action * 2)
+        return state.astype(np.float32), reward, done, info_dict
 
 
-def get_gym_env_args(env, if_print) -> dict:
+def get_gym_env_args(env, if_print) -> dict:  # [ElegantRL.2021.12.12]
+    """Get a dict ``env_args`` about a standard OpenAI gym env information.
+
+    :param env: a standard OpenAI gym env
+    :param if_print: [bool] print the dict about env information.
+    :return: env_args [dict]
+
+    env_args = {
+        'env_num': 1,               # [int] the environment number, 'env_num>1' in vectorized env
+        'env_name': env_name,       # [str] the environment name, such as XxxXxx-v0
+        'max_step': max_step,       # [int] the steps in an episode. (from env.reset to done).
+        'state_dim': state_dim,     # [int] the dimension of state
+        'action_dim': action_dim,   # [int] the dimension of action or the number of discrete action
+        'if_discrete': if_discrete, # [bool] action space is discrete or continuous
+    }
     """
-    Get a dict ``env_args`` about a standard OpenAI gym env information.
 
-    'env_num': 1,
+    env_num = getattr(env, 'env_num') if hasattr(env, 'env_num') else 1
 
-    'env_name': env_name,            # [str] the environment name, such as XxxXxx-v0
-
-    'max_step': max_step,            # [int] the steps in an episode. (from env.reset to done).
-
-    'state_dim': state_dim,          # [int] the dimension of state
-
-    'action_dim': action_dim,        # [int] the dimension of action
-
-    'if_discrete': if_discrete,      # [bool] action space is discrete or continuous
-
-    'target_return': target_return,  # [float] We train agent to reach this target episode return.
-
-    :param env: a standard OpenAI gym env.
-    :param if_print: print the dict about env information.
-    :return: a dict of env_args.
-    """
-    env_num = getattr(env, "env_num") if hasattr(env, "env_num") else 1
-
-    if isinstance(env, gym.Env):
-        env_name = getattr(env, "env_name", None)
+    if {'unwrapped', 'observation_space', 'action_space', 'spec'}.issubset(dir(env)):  # isinstance(env, gym.Env):
+        env_name = getattr(env, 'env_name', None)
         env_name = env.unwrapped.spec.id if env_name is None else env_name
 
         state_shape = env.observation_space.shape
-        state_dim = (
-            state_shape[0] if len(state_shape) == 1 else state_shape
-        )  # sometimes state_dim is a list
+        state_dim = state_shape[0] if len(state_shape) == 1 else state_shape  # sometimes state_dim is a list
 
-        target_return = getattr(env, "target_return", None)
-        target_return_default = getattr(env.spec, "reward_threshold", None)
-        if target_return is None:
-            target_return = target_return_default
-        if target_return is None:
-            target_return = 2**16
-
-        max_step = getattr(env, "max_step", None)
-        max_step_default = getattr(env, "_max_episode_steps", None)
+        max_step = getattr(env, 'max_step', None)
+        max_step_default = getattr(env, '_max_episode_steps', None)
         if max_step is None:
             max_step = max_step_default
         if max_step is None:
-            max_step = 2**10
+            max_step = 2 ** 10
 
         if_discrete = isinstance(env.action_space, gym.spaces.Discrete)
         if if_discrete:  # make sure it is discrete action space
             action_dim = env.action_space.n
-        elif isinstance(
-            env.action_space, gym.spaces.Box
-        ):  # make sure it is continuous action space
+        elif isinstance(env.action_space, gym.spaces.Box):  # make sure it is continuous action space
             action_dim = env.action_space.shape[0]
-            assert not any(env.action_space.high - 1)
-            assert not any(env.action_space.low + 1)
+            if any(env.action_space.high - 1):
+                print('WARNING: env.action_space.high', env.action_space.high)
+            if any(env.action_space.low + 1):
+                print('WARNING: env.action_space.low', env.action_space.low)
         else:
-            raise RuntimeError(
-                "\n| Error in get_gym_env_info()"
-                "\n  Please set these value manually: if_discrete=bool, action_dim=int."
-                "\n  And keep action_space in (-1, 1)."
-            )
+            raise RuntimeError('\n| Error in get_gym_env_info()'
+                               '\n  Please set these value manually: if_discrete=bool, action_dim=int.'
+                               '\n  And keep action_space in (-1, 1).')
     else:
         env_name = env.env_name
         max_step = env.max_step
         state_dim = env.state_dim
         action_dim = env.action_dim
         if_discrete = env.if_discrete
-        target_return = env.target_return
 
-    env_args = {
-        "env_num": env_num,
-        "env_name": env_name,
-        "max_step": max_step,
-        "state_dim": state_dim,
-        "action_dim": action_dim,
-        "if_discrete": if_discrete,
-        "target_return": target_return,
-    }
+    env_args = {'env_num': env_num,
+                'env_name': env_name,
+                'max_step': max_step,
+                'state_dim': state_dim,
+                'action_dim': action_dim,
+                'if_discrete': if_discrete, }
     if if_print:
         env_args_repr = repr(env_args)
-        env_args_repr = env_args_repr.replace(",", ",\n   ")
-        env_args_repr = env_args_repr.replace("{", "{\n    ")
-        env_args_repr = env_args_repr.replace("}", ",\n}")
+        env_args_repr = env_args_repr.replace(',', f",\n{' ':11}").replace('{', "{")
         print(f"env_args = {env_args_repr}")
     return env_args
 
 
-def kwargs_filter(func, kwargs: dict):
-    """
-    Filter the variable in env func.
-
-    :param func: the function for creating an env.
-    :param kwargs: args for the env.
-    :return: filtered args.
-    """
+def kwargs_filter(func, kwargs: dict):  # todo into `build_env()`
     import inspect
 
     sign = inspect.signature(func).parameters.values()
@@ -110,27 +99,11 @@ def kwargs_filter(func, kwargs: dict):
     return {key: kwargs[key] for key in common_args}  # filtered kwargs
 
 
-def build_env(env=None, env_func=None, env_args=None):
-    """
-    Create an environment. If pass an existed env, copy a new one.
-
-    :param env: an existed environment. (please pass None for now)
-    :param env_func: the function for creating an env.
-    :param env_args: the args for the env. Please take look at the demo.
-    :return: an environment.
-    """
-    if env is not None:
-        env = deepcopy(env)
-    elif env_func.__module__ == "gym.envs.registration":
-        import gym
-
-        gym.logger.set_level(40)  # Block warning
-        env = env_func(id=env_args["env_name"])
+def build_env(env_func=None, env_args=None):
+    if env_func.__module__ == 'gym.envs.registration':  # special rule
+        env = env_func(id=env_args['env_name'])
     else:
         env = env_func(**kwargs_filter(env_func.__init__, env_args.copy()))
-
-    env.max_step = env.max_step if hasattr(env, "max_step") else env_args["max_step"]
-    env.if_discrete = (
-        env.if_discrete if hasattr(env, "if_discrete") else env_args["if_discrete"]
-    )
+    for attr_str in ('state_dim', 'action_dim', 'max_step', 'if_discrete'):
+        setattr(env, attr_str, env_args[attr_str])
     return env
--- elegantrl_helloworld/net.py
+++ elegantrl_helloworld/net.py
@@ -1,184 +1,81 @@
-import numpy as np
-import numpy.random as rd
 import torch
 import torch.nn as nn
+import numpy as np
+import numpy.random as rd
 
 
-class QNet(nn.Module):  # nn.Module is a standard PyTorch Network
-    """
-    Class for **Q-network**.
-
-    :param mid_dim[int]: the middle dimension of networks
-    :param state_dim[int]: the dimension of state (the number of state vector)
-    :param action_dim[int]: the dimension of action (the number of discrete action)
-    """
-
-    def __init__(self, mid_dim, state_dim, action_dim):
+class QNet(nn.Module):  # `nn.Module` is a PyTorch module for neural network
+    def __init__(self, mid_dim, mid_layer_num, state_dim, action_dim):  # todo num_mid_layer
         super().__init__()
-        self.net = nn.Sequential(
-            nn.Linear(state_dim, mid_dim),
-            nn.ReLU(),
-            nn.Linear(mid_dim, mid_dim),
-            nn.ReLU(),
-            nn.Linear(mid_dim, mid_dim),
-            nn.ReLU(),
-            nn.Linear(mid_dim, action_dim),
-        )
-        self.explore_rate = 0.125
+        self.net = build_fcn(mid_dim, mid_layer_num, inp_dim=state_dim, out_dim=action_dim)
+
+        self.explore_rate = None
         self.action_dim = action_dim
 
     def forward(self, state):
-        """
-        The forward function.
-
-        :param state: the input state.
-        :return: the output tensor.
-        """
         return self.net(state)  # Q values for multiple actions
 
-    def get_action(self, state):
-        """
-        The forward function with ϵ-greedy.
-
-        :param state: the input state.
-        :return: the action.
-        """
-        return (
-            self.net(state).argmax(dim=1, keepdim=True)
-            if rd.rand() > self.explore_rate
-            else torch.randint(self.action_dim, size=(state.shape[0], 1))
-        )
+    def get_action(self, state):  # todo return [int] the index of discrete action
+        if rd.rand() > self.explore_rate:  # todo self.explore_rate < rd.rand():
+            return self.net(state).argmax(dim=1, keepdim=True)
+        else:
+            action = torch.randint(self.action_dim, size=(state.shape[0], 1))
+        return action
 
 
-class ActorSAC(nn.Module):
-    """
-    Actor class for **SAC** with stochastic, learnable, **state-dependent** log standard deviation..
-
-    :param mid_dim[int]: the middle dimension of networks
-    :param state_dim[int]: the dimension of state (the number of state vector)
-    :param action_dim[int]: the dimension of action (the number of discrete action)
-    """
-
-    def __init__(self, mid_dim, state_dim, action_dim):
+class Actor(nn.Module):  # todo inp --> input, out --> output
+    def __init__(self, mid_dim, mid_layer_num, state_dim, action_dim):
         super().__init__()
-        self.net_state = nn.Sequential(
-            nn.Linear(state_dim, mid_dim),
-            nn.ReLU(),
-            nn.Linear(mid_dim, mid_dim),
-            nn.ReLU(),
-        )
-        self.net_a_avg = nn.Sequential(
-            nn.Linear(mid_dim, mid_dim), nn.ReLU(), nn.Linear(mid_dim, action_dim)
-        )  # the average of action
-        self.net_a_std = nn.Sequential(
-            nn.Linear(mid_dim, mid_dim), nn.ReLU(), nn.Linear(mid_dim, action_dim)
-        )  # the log_std of action
-        self.log_sqrt_2pi = np.log(np.sqrt(2 * np.pi))
+        self.net = build_fcn(mid_dim, mid_layer_num, inp_dim=state_dim, out_dim=action_dim)
+
+        self.explore_noise = None  # standard deviation of exploration action noise
+        # todo explore_noise_std
 
     def forward(self, state):
-        """
-        The forward function for **SAC**.
+        return self.net(state).tanh()  # action
 
-        :param state: the input state.
-        :return: the output tensor.
-        """
-        tmp = self.net_state(state)
-        return self.net_a_avg(tmp).tanh()  # action
+    def get_action(self, state):  # for exploration
+        action = self.net(state).tanh()
+        noise = (torch.randn_like(action) * self.explore_noise).clamp(-0.5, 0.5)
+        return (action + noise).clamp(-1.0, 1.0)
 
-    def get_action(self, state):
-        """
-        The forward function with noise.
 
-        :param state: the input state.
-        :return: the action and added noise.
-        """
-        t_tmp = self.net_state(state)
-        a_avg = self.net_a_avg(t_tmp)  # NOTICE! it is a_avg without .tanh()
-        a_std = self.net_a_std(t_tmp).clamp(-20, 2).exp()
-        return torch.normal(a_avg, a_std).tanh()  # re-parameterize
+class Critic(nn.Module):
+    def __init__(self, mid_dim, mid_layer_num, state_dim, action_dim):
+        super().__init__()
+        self.net = build_fcn(mid_dim, mid_layer_num, inp_dim=state_dim + action_dim, out_dim=1)
 
-    def get_action_logprob(self, state):
-        """
-        Compute the action and log of probability with current network.
-
-        :param state: the input state.
-        :return: the action and log of probability.
-        """
-        t_tmp = self.net_state(state)
-        a_avg = self.net_a_avg(t_tmp)  # NOTICE! it needs a_avg.tanh()
-        a_std_log = self.net_a_std(t_tmp).clamp(-20, 2)
-        a_std = a_std_log.exp()
-
-        noise = torch.randn_like(a_avg, requires_grad=True)
-        a_tan = (a_avg + a_std * noise).tanh()  # action.tanh()
-
-        log_prob = (
-            a_std_log + self.log_sqrt_2pi + noise.pow(2).__mul__(0.5)
-        )  # noise.pow(2) * 0.5
-        log_prob = (
-            log_prob + (-a_tan.pow(2) + 1.000001).log()
-        )  # fix log_prob using the derivative of action.tanh()
-        return a_tan, log_prob.sum(1, keepdim=True)
+    def forward(self, state, action):
+        return self.net(torch.cat((state, action), dim=1))  # Q value
 
 
 class ActorPPO(nn.Module):
-    """
-    Actor class for **PPO** with stochastic, learnable, **state-independent** log standard deviation.
-
-    :param mid_dim[int]: the middle dimension of networks
-    :param state_dim[int]: the dimension of state (the number of state vector)
-    :param action_dim[int]: the dimension of action (the number of discrete action)
-    """
-
-    def __init__(self, mid_dim, state_dim, action_dim):
+    def __init__(self, mid_dim, mid_layer_num, state_dim, action_dim):
         super().__init__()
-        self.net = nn.Sequential(
-            nn.Linear(state_dim, mid_dim),
-            nn.ReLU(),
-            nn.Linear(mid_dim, mid_dim),
-            nn.ReLU(),
-            nn.Linear(mid_dim, mid_dim),
-            nn.ReLU(),
-            nn.Linear(mid_dim, action_dim),
-        )
+        self.net = build_fcn(mid_dim, mid_layer_num, inp_dim=state_dim, out_dim=action_dim)
 
         # the logarithm (log) of standard deviation (std) of action, it is a trainable parameter
-        self.a_std_log = nn.Parameter(
-            torch.zeros((1, action_dim)) - 0.5, requires_grad=True
-        )
+        self.a_std_log = nn.Parameter(torch.zeros((1, action_dim)) - 0.5, requires_grad=True)
+        # todo action_std_log
         self.sqrt_2pi_log = np.log(np.sqrt(2 * np.pi))
 
     def forward(self, state):
-        """
-        The forward function for **PPO**.
-
-        :param state: the input state.
-        :return: the output tensor.
-        """
-        return self.net(state).tanh()  # action.tanh()
+        return self.net(state).tanh()  # action
 
     def get_action(self, state):
-        """
-        The forward function with Gaussian noise.
-
-        :param state: the input state.
-        :return: the action and added noise.
-        """
         a_avg = self.net(state)
         a_std = self.a_std_log.exp()
+        # todo action_avg
 
         noise = torch.randn_like(a_avg)
         action = a_avg + noise * a_std
         return action, noise
 
-    def get_logprob_entropy(self, state, action):
-        """
-        Compute the log of probability with current network.
+    def get_old_logprob(self, _action, noise):
+        delta = noise.pow(2) * 0.5
+        return -(self.a_std_log + self.sqrt_2pi_log + delta).sum(1)  # old_logprob
 
-        :param state: the input state.
-        :param action: the action.
-        :return: the log of probability and entropy.
-        """
+    def get_logprob_entropy(self, state, action):
         a_avg = self.net(state)
         a_std = self.a_std_log.exp()
 
@@ -186,159 +83,25 @@ class ActorPPO(nn.Module):
         logprob = -(self.a_std_log + self.sqrt_2pi_log + delta).sum(1)  # new_logprob
 
         dist_entropy = (logprob.exp() * logprob).mean()  # policy entropy
-        return logprob, dist_entropy
-
-    def get_old_logprob(self, _action, noise):  # noise = action - a_noise
-        """
-        Compute the log of probability with old network.
-
-        :param _action: the action made.
-        :param noise: the noised added.
-        :return: the log of probability of old network.
-        """
-        delta = noise.pow(2) * 0.5
-        return -(self.a_std_log + self.sqrt_2pi_log + delta).sum(1)  # old_logprob
+        return logprob, dist_entropy  # todo entropy
 
     @staticmethod
-    def get_a_to_e(action):
-        return action.tanh()
-
-
-class ActorDiscretePPO(nn.Module):
-    """
-    Actor class for **Discrete PPO**.
-
-    :param mid_dim[int]: the middle dimension of networks
-    :param state_dim[int]: the dimension of state (the number of state vector)
-    :param action_dim[int]: the dimension of action (the number of discrete action)
-    """
-
-    def __init__(self, mid_dim, state_dim, action_dim):
-        super().__init__()
-        self.net = nn.Sequential(
-            nn.Linear(state_dim, mid_dim),
-            nn.ReLU(),
-            nn.Linear(mid_dim, mid_dim),
-            nn.ReLU(),
-            nn.Linear(mid_dim, mid_dim),
-            nn.ReLU(),
-            nn.Linear(mid_dim, action_dim),
-        )
-        self.action_dim = action_dim
-        self.soft_max = nn.Softmax(dim=-1)
-        self.Categorical = torch.distributions.Categorical
-
-    def forward(self, state):
-        """
-        The forward function for **Q-network**.
-
-        :param state: the input state.
-        :return: the output tensor.
-        """
-        return self.net(state)  # action_prob without softmax
-
-    def get_action(self, state):
-        """
-        The forward function with SoftMax.
-
-        :param state: the input state.
-        :return: the action and added noise.
-        """
-        a_prob = self.soft_max(self.net(state))
-        # action = Categorical(a_prob).sample()
-        samples_2d = torch.multinomial(a_prob, num_samples=1, replacement=True)
-        action = samples_2d.reshape(state.size(0))
-        return action, a_prob
-
-    def get_logprob_entropy(self, state, a_int):
-        # assert a_int.shape == (batch_size, 1)
-        a_prob = self.soft_max(self.net(state))
-        dist = self.Categorical(a_prob)
-        return dist.log_prob(a_int.squeeze(1)), dist.entropy().mean()
-
-    def get_old_logprob(self, a_int, a_prob):
-        # assert a_int.shape == (batch_size, 1)
-        dist = self.Categorical(a_prob)
-        return dist.log_prob(a_int.squeeze(1))
-
-    @staticmethod
-    def get_a_to_e(action):
-        return action.int()
+    def get_a_to_e(action):  # convert action of network to action of environment
+        return action.tanh()  # todo convert_action_for_env
 
 
 class CriticPPO(nn.Module):
-    """
-    The Critic class for **PPO**.
-
-    :param mid_dim[int]: the middle dimension of networks
-    :param state_dim[int]: the dimension of state (the number of state vector)
-    :param action_dim[int]: the dimension of action (the number of discrete action)
-    """
-
-    def __init__(self, mid_dim, state_dim, _action_dim):
+    def __init__(self, mid_dim, mid_layer_num, state_dim, _action_dim):
         super().__init__()
-        self.net = nn.Sequential(
-            nn.Linear(state_dim, mid_dim),
-            nn.ReLU(),
-            nn.Linear(mid_dim, mid_dim),
-            nn.ReLU(),
-            nn.Linear(mid_dim, mid_dim),
-            nn.ReLU(),
-            nn.Linear(mid_dim, 1),
-        )
+        self.net = build_fcn(mid_dim, mid_layer_num, inp_dim=state_dim, out_dim=1)
 
     def forward(self, state):
-        """
-        The forward function to ouput the value of the state.
-
-        :param state: the input state.
-        :return: the output tensor.
-        """
         return self.net(state)  # advantage value
 
 
-class CriticTwin(nn.Module):  # shared parameter
-    """
-    The Critic class for **Clipped Double DQN**.
-
-    :param mid_dim[int]: the middle dimension of networks
-    :param state_dim[int]: the dimension of state (the number of state vector)
-    :param action_dim[int]: the dimension of action (the number of discrete action)
-    """
-
-    def __init__(self, mid_dim, state_dim, action_dim):
-        super().__init__()
-        self.net_sa = nn.Sequential(
-            nn.Linear(state_dim + action_dim, mid_dim),
-            nn.ReLU(),
-            nn.Linear(mid_dim, mid_dim),
-            nn.ReLU(),
-        )  # concat(state, action)
-        self.net_q1 = nn.Sequential(
-            nn.Linear(mid_dim, mid_dim), nn.ReLU(), nn.Linear(mid_dim, 1)
-        )  # q1 value
-        self.net_q2 = nn.Sequential(
-            nn.Linear(mid_dim, mid_dim), nn.ReLU(), nn.Linear(mid_dim, 1)
-        )  # q2 value
-
-    def forward(self, state, action):
-        """
-        The forward function to ouput the value of the state.
-
-        :param state: the input state.
-        :param action: the input action.
-        :return: the output tensor.
-        """
-        tmp = self.net_sa(torch.cat((state, action), dim=1))
-        return self.net_q1(tmp)  # one Q value
-
-    def get_q1_q2(self, state, action):
-        """
-        The forward function to output two Q-values from two shared-paramter networks.
-
-        :param state: the input state.
-        :param action: the input action.
-        :return: the output tensor.
-        """
-        tmp = self.net_sa(torch.cat((state, action), dim=1))
-        return self.net_q1(tmp), self.net_q2(tmp)  # two Q values
+def build_fcn(mid_dim, mid_layer_num, inp_dim, out_dim):  # fcn (Fully Connected Network)
+    net_list = [nn.Linear(inp_dim, mid_dim), nn.ReLU(), ]
+    for _ in range(mid_layer_num):
+        net_list += [nn.Linear(mid_dim, mid_dim), nn.ReLU(), ]
+    net_list += [nn.Linear(mid_dim, out_dim), ]
+    return nn.Sequential(*net_list)
