95e4d1a1f12b7aba5d9417d34f6cee0013f013e2
shixun404 <shixun404@gmail.com>
2022-06-07 20:34:02 -0400

    tutorial ppo

--- elegantrl_helloworld/tutorial_PPO.py
+++ elegantrl_helloworld/tutorial_PPO.py
@@ -1,36 +1,20 @@
 from elegantrl_helloworld.config import Arguments
-from elegantrl_helloworld.run import train_agent, evaluate_agent
+from elegantrl_helloworld.run import train_and_evaluate
 from elegantrl_helloworld.env import get_gym_env_args, PendulumEnv
-
+import yaml
 
 def train_ppo_in_pendulum(gpu_id=0):
     env_name = "Pendulum"
     alg = "PPO"
+    with open("config.yml", 'r') as f:
+       hyp = yaml.safe_load(f)[alg][env_name]
     env = PendulumEnv()
     env_func = PendulumEnv
     env_args = get_gym_env_args(env, if_print=True)
 
-    args = Arguments(agent_class, env_func, env_args)
-
-    '''reward shaping'''
-    args.reward_scale = 2 ** -1  # RewardRange: -1800 < -200 < -50 < 0
-    args.gamma = 0.97
-
-    '''network update'''
-    args.target_step = args.max_step * 8
-    args.net_dim = 2 ** 7
-    args.num_layer = 2
-    args.batch_size = 2 ** 8
-    args.repeat_times = 2 ** 5
-
-    '''evaluate'''
-    args.eval_gap = 2 ** 6
-    args.eval_times = 2 ** 3
-    args.break_step = int(8e5)
-
-    args.learner_gpus = gpu_id
-    train_agent(args)
-    evaluate_agent(args)
+    args = Arguments(env_func, env_args, hyp)
+    
+    train_and_evaluate(args)
     
 def train_ppo_in_lunar_lander_or_bipedal_walker(gpu_id=0):
     from elegantrl_helloworld.agent import AgentPPO
