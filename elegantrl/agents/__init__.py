a46c6079c7192e2e0b2db4ff5b6ed0cee63bd136
shixun404 <shixun404@gmail.com>
2022-06-24 06:03:00 -0400

    fix merge bugs

--- elegantrl/agents/__init__.py
+++ elegantrl/agents/__init__.py
@@ -9,15 +9,5 @@ from elegantrl.agents.AgentDuelingDoubleDQN import AgentDuelingDoubleDQN
 # off-policy
 from elegantrl.agents.AgentDDPG import AgentDDPG
 from elegantrl.agents.AgentTD3 import AgentTD3
-<<<<<<< HEAD
 from elegantrl.agents.AgentSAC import AgentSAC, AgentModSAC
 from elegantrl.agents.AgentREDQ import AgentREDQ
-=======
-from elegantrl.agents.AgentSAC import AgentSAC, AgentReSAC
-from elegantrl.agents.AgentSAC import AgentReSACHtermK
-
-# on-policy
-from elegantrl.agents.AgentA2C import AgentA2C, AgentDiscreteA2C
-from elegantrl.agents.AgentPPO import AgentPPO, AgentDiscretePPO
-from elegantrl.agents.AgentPPO import AgentPPOHtermK
->>>>>>> dev2
