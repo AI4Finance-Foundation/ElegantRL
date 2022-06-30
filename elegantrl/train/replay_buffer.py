3f8217706eba9a05a4fbd35b151b64ab99e14bd1
shixun404 <shixun404@gmail.com>
2022-06-30 17:22:16 -0400

    replay buffer list

--- elegantrl/train/replay_buffer.py
+++ elegantrl/train/replay_buffer.py
@@ -194,3 +194,14 @@ class BinarySearchTree:
         prob = prob.cpu().numpy()
         self.update_ids(self.indices, prob)
 
+class ReplayBufferList(list):  # for on-policy
+    def __init__(self):
+        list.__init__(self)
+
+    def update_buffer(self, traj_list):
+        cur_items = list(map(list, zip(*traj_list)))
+        self[:] = [torch.cat(item, dim=0) for item in cur_items]
+
+        steps = self[1].shape[0]
+        r_exp = self[1].mean().item()
+        return steps, r_exp
