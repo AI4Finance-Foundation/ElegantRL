9b19c9b497f42546280b6bd167a020c0374b27f4
shixun404 <shixun404@gmail.com>
2022-06-30 19:18:53 -0400

    dmeo

--- elegantrl/train/run.py
+++ elegantrl/train/run.py
@@ -7,6 +7,8 @@ import multiprocessing as mp
 from elegantrl.train.config import build_env
 from elegantrl.train.evaluator import Evaluator
 from elegantrl.train.replay_buffer import ReplayBuffer
+from elegantrl.train.config import Arguments
+from elegantrl.agents.AgentBase import AgentBase
 
 
 def init_agent(args, gpu_id: int, env=None):
@@ -101,3 +103,181 @@ def train_and_evaluate(args):
     print(f'| UsedTime: {time.time() - evaluator.start_time:.0f} | SavedDir: {cwd}')
 
     agent.save_or_load_agent(cwd, if_save=True)
+
+
+
+
+
+'''train multiple process'''
+
+
+def train_and_evaluate_mp(args: Arguments):
+    args.init_before_training()
+
+    process = list()
+    mp.set_start_method(method='spawn', force=True)  # force all the multiprocessing to 'spawn' methods
+
+    evaluator_pipe = PipeEvaluator()
+    process.append(mp.Process(target=evaluator_pipe.run, args=(args,)))
+
+    worker_pipe = PipeWorker(args.worker_num)
+    process.extend([mp.Process(target=worker_pipe.run, args=(args, worker_id))
+                    for worker_id in range(args.worker_num)])
+
+    learner_pipe = PipeLearner()
+    process.append(mp.Process(target=learner_pipe.run, args=(args, evaluator_pipe, worker_pipe)))
+
+    for p in process:
+        p.start()
+
+    process[-1].join()  # waiting for learner
+    process_safely_terminate(process)
+
+
+class PipeWorker:
+    def __init__(self, worker_num: int):
+        self.worker_num = worker_num
+        self.pipes = [mp.Pipe() for _ in range(worker_num)]
+        self.pipe1s = [pipe[1] for pipe in self.pipes]
+
+    def explore(self, agent: AgentBase):
+        act_dict = agent.act.state_dict()
+
+        for worker_id in range(self.worker_num):
+            self.pipe1s[worker_id].send(act_dict)
+
+        traj_lists = [pipe1.recv() for pipe1 in self.pipe1s]
+        return traj_lists
+
+    def run(self, args: Arguments, worker_id: int):
+        torch.set_grad_enabled(False)
+        gpu_id = args.learner_gpus
+
+        '''init'''
+        env = build_env(args.env, args.env_func, args.env_args)
+        agent = init_agent(args, gpu_id, env)
+
+        '''loop'''
+        target_step = args.target_step
+        if args.if_off_policy:
+            trajectory = agent.explore_env(env, args.target_step)
+            self.pipes[worker_id][0].send(trajectory)
+        del args
+
+        while True:
+            act_dict = self.pipes[worker_id][0].recv()
+            agent.act.load_state_dict(act_dict)
+            trajectory = agent.explore_env(env, target_step)
+            self.pipes[worker_id][0].send(trajectory)
+
+import wandb
+class PipeLearner:
+    def __init__(self):
+        #wandb.init(project="DDPG_H")
+        pass
+        
+
+    @staticmethod
+    def run(args: Arguments, comm_eva: mp.Pipe, comm_exp: mp.Pipe):
+        torch.set_grad_enabled(False)
+        gpu_id = args.learner_gpus
+        cwd = args.cwd
+        #wandb.init(project="DDPG_H")
+
+        '''init'''
+        agent = init_agent(args, gpu_id)
+        buffer = init_buffer(args, gpu_id)
+
+        '''loop'''
+        if_train = True
+        while if_train:
+            traj_list = comm_exp.explore(agent)
+            steps, r_exp = buffer.update_buffer(traj_list)
+
+            torch.set_grad_enabled(True)
+            logging_tuple = agent.update_net(buffer)
+            torch.set_grad_enabled(False)
+            #wandb.log({"obj_cri": logging_tuple[0], "obj_act": logging_tuple[1]})
+            if_train, if_save = comm_eva.evaluate_and_save_mp(agent.act, steps, r_exp, logging_tuple)
+        agent.save_or_load_agent(cwd, if_save=True)
+        print(f'| Learner: Save in {cwd}')
+
+        env = build_env(env_func=args.env_func, env_args=args.env_args)
+        buffer.get_state_norm(
+            cwd=cwd,
+            state_avg=getattr(env, 'state_avg', 0.0),
+            state_std=getattr(env, 'state_std', 1.0),
+        )
+        if hasattr(buffer, 'save_or_load_history'):
+            print(f"| LearnerPipe.run: ReplayBuffer saving in {cwd}")
+            buffer.save_or_load_history(cwd, if_save=True)
+
+
+class PipeEvaluator:
+    def __init__(self):
+        self.pipe0, self.pipe1 = mp.Pipe()
+
+    def evaluate_and_save_mp(self, act, steps: int, r_exp: float, logging_tuple: tuple) -> (bool, bool):
+        if self.pipe1.poll():  # if_evaluator_idle
+            if_train, if_save_agent = self.pipe1.recv()
+            act_state_dict = act.state_dict().copy()  # deepcopy(act.state_dict())
+        else:
+            if_train = True
+            if_save_agent = False
+            act_state_dict = None
+
+        self.pipe1.send((act_state_dict, steps, r_exp, logging_tuple))
+        return if_train, if_save_agent
+
+    def run(self, args: Arguments):
+        torch.set_grad_enabled(False)
+        gpu_id = args.learner_gpus
+
+        '''init'''
+        agent = init_agent(args, gpu_id)
+        evaluator = init_evaluator(args, gpu_id)
+
+        '''loop'''
+        cwd = args.cwd
+        act = agent.act
+        break_step = args.break_step
+        if_allow_break = args.if_allow_break
+        save_gap = args.save_gap
+        del args
+
+        if_save = False
+        if_train = True
+        if_reach_goal = False
+        save_counter = 0
+        while if_train:
+            act_dict, steps, r_exp, logging_tuple = self.pipe0.recv()
+
+            if act_dict:
+                act.load_state_dict(act_dict)
+                if_reach_goal, if_save = evaluator.evaluate_save_and_plot(act, steps, r_exp, logging_tuple)
+
+                save_counter += 1
+                if save_counter == save_gap:
+                    save_counter = 0
+                    torch.save(act.state_dict(), f"{cwd}/actor_{evaluator.total_step:012}.pth")
+            else:
+                evaluator.total_step += steps
+
+            if_train = not ((if_allow_break and if_reach_goal)
+                            or evaluator.total_step > break_step
+                            or os.path.exists(f'{cwd}/stop'))
+            self.pipe0.send((if_train, if_save))
+
+        print(f'| UsedTime: {time.time() - evaluator.start_time:>7.0f} | SavedDir: {cwd}')
+
+        while True:  # wait for the forced stop from main process
+            self.pipe0.recv()
+            self.pipe0.send((False, False))
+
+
+def process_safely_terminate(process: list):
+    for p in process:
+        try:
+            p.kill()
+        except OSError as e:
+            print(e)
