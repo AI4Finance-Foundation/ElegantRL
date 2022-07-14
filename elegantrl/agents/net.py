e9eb4aa0a9bb4a4ed9a6c5c42df33b8bd5d6c4ca
shixun404 <shixun404@gmail.com>
2022-07-14 14:44:25 -0400

    actor

--- elegantrl/agents/net.py
+++ elegantrl/agents/net.py
@@ -193,32 +193,47 @@ class QNetTwinDuel(nn.Module):  # D3QN: Dueling Double DQN
 """Actor (policy network)"""
 
 
+
 class Actor(nn.Module):
-    def __init__(self, mid_dim, state_dim, action_dim):
+    def __init__(self, mid_dim: int, num_layer: int, state_dim: int, action_dim: int):
         super().__init__()
-        self.net = nn.Sequential(
-            nn.Linear(state_dim, mid_dim),
-            nn.ReLU(),
-            nn.Linear(mid_dim, mid_dim),
-            nn.ReLU(),
-            nn.Linear(mid_dim, mid_dim),
-            nn.ReLU(),
-            nn.Linear(mid_dim, action_dim),
-        )
-        self.explore_noise = 0.1  # standard deviation of exploration action noise
+        self.net = build_mlp(mid_dim, num_layer, input_dim=state_dim, output_dim=action_dim)
+        self.explore_noise_std = 0.1  # standard deviation of exploration action noise
+        self.log_sqrt_2pi = np.log(np.sqrt(2 * np.pi))
 
-    def forward(self, state):
-        return self.net(state).tanh()  # action.tanh()
+    def forward(self, state: Tensor) -> Tensor:
+        return self.net(state).tanh()  # action
 
-    def get_action(self, state):  # for exploration
+    def get_action(self, state: Tensor) -> Tensor:  # for exploration
         action = self.net(state).tanh()
-        noise = (torch.randn_like(action) * self.explore_noise).clamp(-0.5, 0.5)
+        noise = (torch.randn_like(action) * self.explore_noise_std).clamp(-0.5, 0.5)
         return (action + noise).clamp(-1.0, 1.0)
 
-    def get_action_noise(self, state, action_std):
+    def get_action_noise(self, state: Tensor, action_std: float) -> Tensor:
         action = self.net(state).tanh()
         noise = (torch.randn_like(action) * action_std).clamp(-0.5, 0.5)
         return (action + noise).clamp(-1.0, 1.0)
+
+    def get_logprob(self, state: Tensor, action: Tensor) -> Tensor:
+        action_avg = self.net(state)
+        action_std = torch.ones_like(action_avg) * self.explore_noise_std
+        action_std_log = action_std.log()
+
+        delta = ((action_avg - action) / action_std).pow(2).__mul__(0.5)
+        logprob = -(action_std_log + self.log_sqrt_2pi + delta)  # new_logprob
+        return logprob
+
+    def get_logprob_fixed(self, state: Tensor, action: Tensor) -> Tensor:
+        action_avg = self.net(state)  # NOTICE! `action_avg` is a tensor without .tanh()
+        action_std = self.explore_noise_std
+        action_std_log = np.log(action_std)  # assert isinstance(action_std, float)
+
+        action_tanh = action_avg.tanh()  # action.tanh()
+
+        logprob = action_std_log + self.log_sqrt_2pi + (action_tanh - action).pow(2).__mul__(0.5)
+        logprob += (-action_tanh.pow(2) + 1.000001).log()  # fix logprob using the derivative of action.tanh()
+        return logprob
+
 
 
 class ActorSAC(nn.Module):
