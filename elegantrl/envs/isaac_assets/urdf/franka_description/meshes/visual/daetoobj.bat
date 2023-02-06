af35673ae6de570033d320e38adccf1fe5a09864
YonV1943 曾伊言 <37322666+Yonv1943@users.noreply.github.com>
2023-02-06 16:18:56 +0800

    isaac gym files not need to save in envs folder

--- elegantrl/envs/isaac_assets/urdf/franka_description/meshes/visual/daetoobj.bat
+++
@@ -1,5 +0,0 @@
-SET PATH=%PATH%;C:/Tools/Assimp/bin/x64/
-forfiles /m *.dae /c "cmd /c assimp export @file @fname.obj --verbose --show-log -ptv"
-
-REM SET PATH=%PATH%;C:/Program Files/VCG/MeshLab/
-REM forfiles /m *.dae /c "cmd /c meshlabserver -i @file -o @fname.obj -m vn vt
