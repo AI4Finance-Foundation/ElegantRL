dc1bcf57174665a90e91e1decd05479333386fea
Adhi Setiawan <adhisetiawan518@gmail.com>
2023-07-17 13:00:40 +0700

    fix relative import error

--- helloworld/run.py
+++ helloworld/run.py
@@ -2,8 +2,8 @@ import os
 import time
 import torch
 import numpy as np
-from .config import Config, build_env
-from .agent import ReplayBuffer
+from config import Config, build_env
+from agent import ReplayBuffer
 
 
 def train_agent(args: Config):
