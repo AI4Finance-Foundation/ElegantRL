9833ff5a80f746a6c4d1bfef081564e5b2ab7877
Ming Zhu <zhumingpassional@gmail.com>
2024-08-24 22:46:33 +0800

    add

--- elegantrl/RLSolver/methods/mcpg.py
+++ elegantrl/RLSolver/methods/mcpg.py
@@ -4,9 +4,9 @@ import sys
 sys.path.append('..')
 from torch_geometric.data import Data
 from L2A.evaluator import EncoderBase64
-from methods.L2A.maxcut_simulator import SimulatorMaxcut, load_graph_list
+from elegantrl.RLSolver.methods.L2A.maxcut_simulator import SimulatorMaxcut, load_graph_list
 import time
-from methods.L2A.maxcut_local_search import SolverLocalSearch
+from elegantrl.RLSolver.methods.L2A.maxcut_local_search import SolverLocalSearch
 
 """
 pip install torch_geometric
