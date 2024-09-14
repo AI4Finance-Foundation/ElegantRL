468f0a83b4385666daa96322c351255cae51a099
Qingpeng Wang <3052148864@qq.com>
2024-09-14 19:54:58 +0800

    update config

--- rlsolver/methods/iSCO/config/maxcut_config.py
+++ rlsolver/methods/iSCO/config/maxcut_config.py
@@ -1,9 +1,9 @@
 import torch as th
 INIT_TEMPERATURE = 1.0
 FINAL_TEMPERATURE = 0
-CHAIN_LENGTH = 10
+CHAIN_LENGTH = 20000
 BATCH_SIZE=1
-DATAPATH = "../../../rlsolver/data/gset/gset_14.txt"
+DATAPATH = "../../../rlsolver/data/syn_BA/barabasi_albert_100_ID0.txt"
 GPU_ID = 0
 def calc_device(gpu_id: int):
     return th.device(f'cuda:{gpu_id}' if th.cuda.is_available() and gpu_id >= 0 else 'cpu')
