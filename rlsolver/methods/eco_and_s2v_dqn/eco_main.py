6cb933dd3357fa7a7d79823f39b3aab6a6d4949b
Ming Zhu <zhumingpassional@gmail.com>
2024-10-26 09:38:11 +0800

    revise

--- rlsolver/methods/eco_and_s2v_dqn/eco_main.py
+++ rlsolver/methods/eco_and_s2v_dqn/eco_main.py
@@ -1,5 +1,6 @@
 import sys
 import os
+
 cur_path = os.path.dirname(os.path.abspath(__file__))
 rlsolver_path = os.path.join(cur_path, '../../../rlsolver')
 sys.path.append(os.path.dirname(rlsolver_path))
@@ -9,18 +10,15 @@ import rlsolver.methods.eco_and_s2v_dqn.train_and_test.train_eco as train
 
 from rlsolver.methods.eco_and_s2v_dqn.config.eco_config import *
 
-just_test = JUSTTEST
-#这也是test中保存结果的地方
+just_test = True
+# 这也是test中保存结果的地方
 # save_loc=f"../../result/{GRAPH_TYPE}_{NODES}spin_eco"
-save_loc=f"../../result"
-
-
+save_loc = f"../../result"
 
 if not just_test:
     train.run(save_loc, graph_save_loc=GRAPHSAVELOC)
-    test.run(save_loc, graph_save_loc=GRAPHSAVELOC, batched=True, max_batch_size=None,just_test=False)
+    test.run(save_loc, graph_save_loc=GRAPHSAVELOC, batched=True, max_batch_size=None, just_test=False)
 
 else:
-    test.run(save_loc, network_save_path = NETWORKSAVEPATH,
+    test.run(save_loc, network_save_path=NETWORKSAVEPATH,
              graph_save_loc=GRAPHSAVELOC, batched=True, max_batch_size=None)
-
