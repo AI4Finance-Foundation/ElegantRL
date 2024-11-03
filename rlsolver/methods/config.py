856bb80dbd666ac2e2ed7c039f9c6b7b0376e69d
Ming Zhu <zhumingpassional@gmail.com>
2024-11-03 14:52:14 +0800

    revise

--- rlsolver/methods/config.py
+++ rlsolver/methods/config.py
@@ -20,9 +20,9 @@ PROBLEM = Problem.maxcut
 
 @unique
 class GraphDistriType(Enum):
+    barabasi_albert: str = "ER"  # 'barabasi_albert'
     erdos_renyi: str = "ER"  # 'erdos_renyi'
     powerlaw: str = "PL"  # 'powerlaw'
-    barabasi_albert: str = "ER"  # 'barabasi_albert'
 
 def calc_device(gpu_id: int):
     return th.device(f'cuda:{gpu_id}' if th.cuda.is_available() and gpu_id >= 0 else 'cpu')
