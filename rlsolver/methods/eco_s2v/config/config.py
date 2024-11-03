7dabac545fd78b2d694a94d95659b7625ee46e26
Ming Zhu <zhumingpassional@gmail.com>
2024-11-03 15:11:58 +0800

    revise

--- rlsolver/methods/eco_s2v/config/config.py
+++ rlsolver/methods/eco_s2v/config/config.py
@@ -9,8 +9,10 @@ from rlsolver.methods.config import GraphType
 # GRAPH_TYPE = 'BA'
 GRAPH_TYPE = GraphType.BA
 
+
 NUM_INFERENCE_NODES = [100, 200, 300, 400, 500, 600, 700, 800, 900, 1000, 1100, 1200, 2000, 3000, 4000, 5000]
 INFERENCE_PREFIXES = [GRAPH_TYPE.value + "_" + str(i) + "_" for i in NUM_INFERENCE_NODES]
+
 # PREFIXES = ["BA_100_", "BA_200_", "BA_300_", "BA_400_", "BA_500_", "BA_600_", "BA_700_", "BA_800_", "BA_900_",
 #             "BA_1000_", "BA_1100_", "BA_1200_", "BA_2000_", "BA_3000_", "BA_4000_",
 #             "BA_5000_"]  # Replace with your desired prefixes
@@ -20,11 +22,14 @@ def calc_device(gpu_id: int):
     return th.device(f'cuda:{gpu_id}' if th.cuda.is_available() and gpu_id >= 0 else 'cpu')
 
 
-ALG_NAME = 'eco'
+ALG_NAME = "eco"
 assert ALG_NAME in ["eco", "s2v"]
 
-NETWORK_SAVE_PATH = 'pretrained_agent/eco/network_best_BA_20spin.pth'
-GRAPH_SAVE_LOC = "../../data/syn_BA"
+# NETWORK_SAVE_PATH = "pretrained_agent/eco/network_best_BA_20spin.pth"
+NETWORK_SAVE_PATH = "pretrained_agent/" + ALG_NAME + "/network_best_" + GRAPH_TYPE.value + "_" + str(NUM_INFERENCE_NODES) + "spin.pth"
+
+# GRAPH_SAVE_LOC = "../../data/syn_BA"
+GRAPH_SAVE_LOC = "../../data/syn_" + GRAPH_TYPE.value
 
 DEVICE = calc_device(GPU_ID)
 TEST_DEVICE = calc_device(-1)
