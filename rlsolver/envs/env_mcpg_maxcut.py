97e60a749b5788c635fb531440ac2d774208ac85
zhumingpassional <zhumingpassional@gmail.com>
2024-11-05 10:37:10 +0800

    revise mcpg, envs

--- rlsolver/envs/env_mcpg_maxcut.py
+++ rlsolver/envs/env_mcpg_maxcut.py
@@ -15,14 +15,12 @@ from rlsolver.methods.util_read_data import (load_graph_list, GraphList,
                                              obtain_num_nodes,
                                              update_xs_by_vs,)
 from rlsolver.methods.util import gpu_info_str, evolutionary_replacement
-from rlsolver.methods.util_evaluator import  Evaluator
-
+from rlsolver.methods.config import *
 TEN = th.Tensor
 
-
 class SimulatorMaxcut:
-    def __init__(self, sim_name: str = 'max_cut', graph_list: GraphList = (),
-                 device=th.device('cpu'), if_bidirectional: bool = False):
+    def __init__(self, sim_name: str = 'max_cut', graph_list: GraphList = [],
+                 device=calc_device(GPU_ID), if_bidirectional: bool = False):
         self.device = device
         self.sim_name = sim_name
         self.int_type = int_type = th.long
@@ -246,66 +244,7 @@ TEN = th.Tensor
 '''local search'''
 
 
-class LocalSearch:
-    def __init__(self, simulator: SimulatorMaxcut, num_nodes: int):
-        self.simulator = simulator
-        self.num_nodes = num_nodes
 
-        self.num_sims = 0
-        self.good_xs = th.tensor([])  # solution x
-        self.good_vs = th.tensor([])  # objective value
-
-    def reset(self, xs: TEN):
-        vs = self.simulator.calculate_obj_values(xs=xs)
-
-        self.good_xs = xs
-        self.good_vs = vs
-        self.num_sims = xs.shape[0]
-        return vs
-
-    def reset_search(self, num_sims):
-        xs = th.empty((num_sims, self.num_nodes), dtype=th.bool, device=self.simulator.device)
-        for sim_id in range(num_sims):
-            _xs = self.simulator.generate_xs_randomly(num_sims=num_sims)
-            _vs = self.simulator.calculate_obj_values(_xs)
-            xs[sim_id] = _xs[_vs.argmax()]
-        return xs
-
-    #self.simulator是并行
-    def random_search(self, num_iters: int = 8, num_spin: int = 8, noise_std: float = 0.3):
-        sim = self.simulator
-        kth = self.num_nodes - num_spin
-
-        prev_xs = self.good_xs.clone()
-        prev_vs_raw = sim.calculate_obj_values_for_loop(prev_xs, if_sum=False)
-        prev_vs = prev_vs_raw.sum(dim=1)
-
-        thresh = None
-        for _ in range(num_iters):
-            '''flip randomly with ws(weights)'''
-            ws = sim.n0_num_n1 - (4 if sim.if_bidirectional else 2) * prev_vs_raw
-            ws_std = ws.max(dim=0, keepdim=True)[0] - ws.min(dim=0, keepdim=True)[0]
-
-            spin_rand = ws + th.randn_like(ws, dtype=th.float32) * (ws_std.float() * noise_std)
-            thresh = th.kthvalue(spin_rand, k=kth, dim=1)[0][:, None] if thresh is None else thresh
-            spin_mask = spin_rand.gt(thresh)
-
-            xs = prev_xs.clone()
-            xs[spin_mask] = th.logical_not(xs[spin_mask])
-            vs = sim.calculate_obj_values(xs)
-
-            update_xs_by_vs(prev_xs, prev_vs, xs, vs)
-
-        '''addition'''
-        for i in range(sim.num_nodes):
-            xs1 = prev_xs.clone()
-            xs1[:, i] = th.logical_not(xs1[:, i])
-            vs1 = sim.calculate_obj_values(xs1)
-
-            update_xs_by_vs(prev_xs, prev_vs, xs1, vs1)
-
-        num_update = update_xs_by_vs(self.good_xs, self.good_vs, prev_xs, prev_vs)
-        return self.good_xs, self.good_vs, num_update
 
 
 def update_xs_by_vs(xs0, vs0, xs1, vs1, if_maximize: bool = True):
